8fb3892bd3381c4f326875f9eda876f4086ea9ec
zxr <991316914@qq.com>
2011-08-05 18:00:50 +0800

    没修改过

--- bc-identity/src/main/java/cn/bc/identity/domain/RichFileEntityImpl.java
+++ bc-identity/src/main/java/cn/bc/identity/domain/RichFileEntityImpl.java
@@ -36,45 +36,5 @@ public abstract class RichFileEntityImpl extends FileEntityImpl implements
 		this.status = status;
 	}
 
-//	/**
-//	 * 获取用于计算文档年月日的日期指
-//	 * 
-//	 * @return
-//	 */
-//	@Transient
-//	protected Calendar getSpecalDate() {
-//		return this.getFileDate();
-//	}
-//
-//	/** 统计用：文档时间的年度 */
-//	@Column(name = "FILE_YEAR")
-//	public int getFileYear() {
-//		return getSpecalDate() != null ? getSpecalDate().get(Calendar.YEAR) : 0;
-//	}
-//
-//	public void setFileYear(int n) {
-//		// do nothing
-//	}
-//
-//	/** 统计用：文档时间的月份(1-12) */
-//	@Column(name = "FILE_MONTH")
-//	public int getFileMonth() {
-//		return getSpecalDate() != null ? getSpecalDate().get(Calendar.MONTH) + 1
-//				: 0;
-//	}
-//
-//	public void setFileMonth(int n) {
-//		// do nothing
-//	}
-//
-//	/** 统计用：文档时间的日(1-31) */
-//	@Column(name = "FILE_DAY")
-//	public int getFileDay() {
-//		return getSpecalDate() != null ? getSpecalDate().get(
-//				Calendar.DAY_OF_MONTH) : 0;
-//	}
-//
-//	public void setFileDay(int n) {
-//		// do nothing
-//	}
+
 }
