08763a15309d41313264836f1947ebe63407ae90
rongjihuang <rongjihuang@gmail.com>
2011-09-20 19:36:44 +0800

    添加用于测试的长时请求action

--- bc-web-struts2/src/main/java/cn/bc/web/struts2/ErrorAction.java
+++ bc-web-struts2/src/main/java/cn/bc/web/struts2/ErrorAction.java
@@ -23,10 +23,24 @@ public class ErrorAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 	protected Log logger = LogFactory.getLog(getClass());
 
-	public String html;
+	public String html = "<div style='margin:10px;font-size:20px;font-weight:bold;'>功能建设中...</div>";
 
 	public String todo() {
-		html = "<div style='margin:10px;font-size:20px;font-weight:bold;'>功能建设中...</div>";
+		return SUCCESS;
+	}
+
+	// 等待的时间,单位为秒
+	public int t = 5;
+
+	public String waiting() throws Exception {
+		if (t > 0) {
+			logger.warn("线程等待" + t + "秒后再继续...");
+			Thread.sleep(t * 1000);
+			logger.warn("线程等待完毕!");
+		}
+
+		html = "<div style='margin:10px;font-size:20px;font-weight:bold;'>祝贺你，加载完毕了！("
+				+ t + "秒)</div>";
 		return SUCCESS;
 	}
 }
