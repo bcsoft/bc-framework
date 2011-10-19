12aac600a126613e1879351634c5c322f5f1cf90
rongjihuang <rongjihuang@gmail.com>
2011-10-19 18:25:33 +0800

    选择Actor性能优化

--- bc-identity/src/main/java/cn/bc/identity/dao/hibernate/jpa/ActorDaoImpl.java
+++ bc-identity/src/main/java/cn/bc/identity/dao/hibernate/jpa/ActorDaoImpl.java
@@ -2,8 +2,10 @@ package cn.bc.identity.dao.hibernate.jpa;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
@@ -17,6 +19,7 @@ import cn.bc.core.query.condition.impl.AndCondition;
 import cn.bc.core.query.condition.impl.EqualsCondition;
 import cn.bc.core.query.condition.impl.InCondition;
 import cn.bc.core.query.condition.impl.OrderCondition;
+import cn.bc.db.jdbc.RowMapper;
 import cn.bc.identity.dao.ActorDao;
 import cn.bc.identity.dao.ActorRelationDao;
 import cn.bc.identity.domain.Actor;
@@ -24,6 +27,7 @@ import cn.bc.identity.domain.ActorHistory;
 import cn.bc.identity.domain.ActorRelation;
 import cn.bc.identity.domain.Resource;
 import cn.bc.orm.hibernate.jpa.HibernateCrudJpaDao;
+import cn.bc.orm.hibernate.jpa.HibernateJpaNativeQuery;
 
 /**
  * 参与者Service接口的实现
@@ -558,5 +562,72 @@ public class ActorDaoImpl extends HibernateCrudJpaDao<Actor> implements
 					+ StringUtils.collectionToCommaDelimitedString(args));
 		}
 		return this.getJpaTemplate().find(hql.toString(), args.toArray());
+	}
+
+	public List<Map<String, String>> find4option(Integer[] actorTypes,
+			Integer[] actorStatues) {
+		ArrayList<Object> args = new ArrayList<Object>();
+		StringBuffer hql = new StringBuffer();
+		hql.append("select a.id,a.type_,a.code,a.name,a.pcode,a.pname");
+		hql.append(" from BC_IDENTITY_ACTOR a");
+
+		boolean isWhere = true;
+		// 类型
+		if (actorTypes != null && actorTypes.length > 0) {
+			isWhere = false;
+			if (actorTypes.length == 1) {
+				hql.append(" where a.type_=?");
+				args.add(actorTypes[0]);
+			} else {
+				hql.append(" where a.type_ in (?");
+				args.add(actorTypes[0]);
+				for (int i = 1; i < actorTypes.length; i++) {
+					hql.append(",?");
+					args.add(actorTypes[i]);
+				}
+				hql.append(")");
+			}
+		}
+
+		// 状态
+		if (actorStatues != null && actorStatues.length > 0) {
+			if (actorStatues.length == 1) {
+				hql.append((isWhere ? " where" : " and") + " a.status_=?");
+				args.add(actorStatues[0]);
+			} else {
+				hql.append((isWhere ? " where" : " and") + " a.status_ in (?");
+				args.add(actorStatues[0]);
+				for (int i = 1; i < actorStatues.length; i++) {
+					hql.append(",?");
+					args.add(actorStatues[i]);
+				}
+				hql.append(")");
+			}
+		}
+
+		// 排序
+		hql.append(" order by a.order_");
+		if (logger.isDebugEnabled()) {
+			logger.debug("hql=" + hql.toString());
+			logger.debug("args="
+					+ StringUtils.collectionToCommaDelimitedString(args));
+		}
+
+		return HibernateJpaNativeQuery.executeNativeSql(getJpaTemplate(),
+				hql.toString(), args.toArray(),
+				new RowMapper<Map<String, String>>() {
+					public Map<String, String> mapRow(Object[] rs, int rowNum) {
+						Map<String, String> map = new HashMap<String, String>();
+						int i = 0;
+						map.put("id", rs[i++].toString());
+						map.put("type_", rs[i++].toString());
+						map.put("code", rs[i++].toString());
+						map.put("name", rs[i++].toString());
+						map.put("pcode", rs[i] != null ? rs[i].toString() : "");
+						i++;
+						map.put("pname", rs[i] != null ? rs[i].toString() : "");
+						return map;
+					}
+				});
 	}
 }
--- bc-identity/src/main/java/cn/bc/identity/service/ActorService.java
+++ bc-identity/src/main/java/cn/bc/identity/service/ActorService.java
@@ -1,6 +1,7 @@
 package cn.bc.identity.service;
 
 import java.util.List;
+import java.util.Map;
 
 import cn.bc.core.service.CrudService;
 import cn.bc.identity.domain.Actor;
@@ -9,113 +10,158 @@ import cn.bc.identity.domain.Resource;
 
 /**
  * 参与者Service接口
+ * 
  * @author dragon
- *
+ * 
  */
-public interface ActorService extends CrudService<Actor>{
+public interface ActorService extends CrudService<Actor> {
 	/**
 	 * 根据编码获取，如用户的登录名
-	 * @param actorCode actor的编码
+	 * 
+	 * @param actorCode
+	 *            actor的编码
 	 * @return
 	 */
 	Actor loadByCode(String actorCode);
 
 	/**
 	 * 获取从属方的单一上级
-	 * @param followerId 从属方id
-	 * @param masterTypes 主控方的类型，对应Actor的type属性
+	 * 
+	 * @param followerId
+	 *            从属方id
+	 * @param masterTypes
+	 *            主控方的类型，对应Actor的type属性
 	 * @return
 	 */
 	Actor loadBelong(Long followerId, Integer[] masterTypes);
 
 	/**
 	 * 获取从属方的所有上级
-	 * @param followerId 从属方id
-	 * @param masterTypes 主控方的类型，对应Actor的type属性
+	 * 
+	 * @param followerId
+	 *            从属方id
+	 * @param masterTypes
+	 *            主控方的类型，对应Actor的type属性
 	 * @return
 	 */
 	List<Actor> findBelong(Long followerId, Integer[] masterTypes);
 
 	/**
 	 * 获取从属方指定关联关系的主控方
-	 * @param followerId 从属方id
-	 * @param relationTypes 关联的类型，对应ActorRelation的type属性
-	 * @param masterTypes 主控方的类型，对应Actor的type属性
+	 * 
+	 * @param followerId
+	 *            从属方id
+	 * @param relationTypes
+	 *            关联的类型，对应ActorRelation的type属性
+	 * @param masterTypes
+	 *            主控方的类型，对应Actor的type属性
 	 * @return
 	 */
-	List<Actor> findMaster(Long followerId, Integer[] relationTypes, Integer[] masterTypes);
-	
+	List<Actor> findMaster(Long followerId, Integer[] relationTypes,
+			Integer[] masterTypes);
+
 	/**
 	 * 获取隶属指定actor的所有actor
-	 * @param masterId 主控方id
-	 * @param relationTypes 关联的类型，对应ActorRelation的type属性
-	 * @param followerTypes 从属方的类型，对应Actor的type属性
+	 * 
+	 * @param masterId
+	 *            主控方id
+	 * @param relationTypes
+	 *            关联的类型，对应ActorRelation的type属性
+	 * @param followerTypes
+	 *            从属方的类型，对应Actor的type属性
 	 * @return
 	 */
-	List<Actor> findFollower(Long masterId, Integer[] relationTypes, Integer[] followerTypes);
+	List<Actor> findFollower(Long masterId, Integer[] relationTypes,
+			Integer[] followerTypes);
 
 	/**
 	 * 获取顶层单位信息
+	 * 
 	 * @return
 	 */
 	List<Actor> findTopUnit();
 
 	/**
 	 * 获取所有单位信息
-	 * @param statues 状态列表
+	 * 
+	 * @param statues
+	 *            状态列表
 	 * @return
 	 */
 	List<Actor> findAllUnit(Integer... statues);
 
 	/**
 	 * 获取隶属关系中的所有直接下级组织信息
-	 * @param higherOrganizationId 上级组织的id
-	 * @param lowerOrganizationTypes 下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
+	 * 
+	 * @param higherOrganizationId
+	 *            上级组织的id
+	 * @param lowerOrganizationTypes
+	 *            下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
 	 * @return
 	 */
-	List<Actor> findLowerOrganization(Long higherOrganizationId, Integer... lowerOrganizationTypes);
+	List<Actor> findLowerOrganization(Long higherOrganizationId,
+			Integer... lowerOrganizationTypes);
 
 	/**
 	 * 获取隶属关系中的所有直接上级组织信息
-	 * @param lowerOrganizationId 下级组织的id
-	 * @param higherOrganizationTypes 上级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
+	 * 
+	 * @param lowerOrganizationId
+	 *            下级组织的id
+	 * @param higherOrganizationTypes
+	 *            上级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
 	 * @return
 	 */
-	List<Actor> findHigherOrganization(Long lowerOrganizationId, Integer... higherOrganizationTypes);
+	List<Actor> findHigherOrganization(Long lowerOrganizationId,
+			Integer... higherOrganizationTypes);
 
 	/**
 	 * 获取直接隶属于组织的人员信息
-	 * @param organizationId 上级组织的id
+	 * 
+	 * @param organizationId
+	 *            上级组织的id
 	 * @return
 	 */
 	List<Actor> findUser(Long organizationId);
 
 	/**
 	 * 获取隶属关系中的所有上级组织信息，包括上级的上级,按祖先的级别排好序
-	 * @param lowerOrganizationId 下级组织的id
-	 * @param ancestorOrganizationTypes 上级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
+	 * 
+	 * @param lowerOrganizationId
+	 *            下级组织的id
+	 * @param ancestorOrganizationTypes
+	 *            上级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
 	 * @return
 	 */
-	List<Actor> findAncestorOrganization(Long lowerOrganizationId, Integer... ancestorOrganizationTypes);
+	List<Actor> findAncestorOrganization(Long lowerOrganizationId,
+			Integer... ancestorOrganizationTypes);
 
 	/**
 	 * 获取隶属关系中的所有下级组织信息，包括下级的下级
-	 * @param higherOrganizationId 上级组织的id
-	 * @param descendantOrganizationTypes 下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
+	 * 
+	 * @param higherOrganizationId
+	 *            上级组织的id
+	 * @param descendantOrganizationTypes
+	 *            下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
 	 * @return
 	 */
-	List<Actor> findDescendantOrganization(Long higherOrganizationId, Integer... descendantOrganizationTypes);
+	List<Actor> findDescendantOrganization(Long higherOrganizationId,
+			Integer... descendantOrganizationTypes);
 
 	/**
 	 * 获取隶属于组织的人员信息，包括组织的所有后代组织下的人员
-	 * @param organizationId 上级组织的id
-	 * @param descendantOrganizationTypes 下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
+	 * 
+	 * @param organizationId
+	 *            上级组织的id
+	 * @param descendantOrganizationTypes
+	 *            下级组织的类型，对应Actor的type属性，默认为单位+部门+岗位
 	 * @return
 	 */
-	List<Actor> findDescendantUser(Long organizationId, Integer... descendantOrganizationTypes);
+	List<Actor> findDescendantUser(Long organizationId,
+			Integer... descendantOrganizationTypes);
 
 	/**
 	 * 获取actor可以使用的模块列表
+	 * 
 	 * @param actorId
 	 * @return
 	 */
@@ -123,27 +169,57 @@ public interface ActorService extends CrudService<Actor>{
 
 	/**
 	 * @param follower
-	 * @param belongId 隶属上级
+	 * @param belongId
+	 *            隶属上级
 	 */
 	Actor save4belong(Actor follower, Long belongId);
 
 	/**
 	 * @param follower
-	 * @param belongIds 隶属上级
+	 * @param belongIds
+	 *            隶属上级
 	 */
 	Actor save4belong(Actor follower, Long[] belongIds);
 
-	/**获取指定类型和状态的Actor
-	 * @param actorTypes 类型列表
-	 * @param actorStatues 状态列表
+	/**
+	 * 获取指定类型和状态的Actor
+	 * 
+	 * @param actorTypes
+	 *            类型列表
+	 * @param actorStatues
+	 *            状态列表
 	 * @return
 	 */
 	List<Actor> find(Integer[] actorTypes, Integer[] actorStatues);
 
-	/**获取指定类型和状态的Actor的ActorHistory
-	 * @param actorTypes 类型列表
-	 * @param actorStatues 状态列表
+	/**
+	 * 获取指定类型和状态的Actor的ActorHistory
+	 * 
+	 * @param actorTypes
+	 *            类型列表
+	 * @param actorStatues
+	 *            状态列表
 	 * @return
 	 */
 	List<ActorHistory> findHistory(Integer[] actorTypes, Integer[] actorStatues);
+
+	/**
+	 * 获取指定类型和状态的Actor的ActorHistory信息
+	 * 
+	 * @param actorTypes
+	 *            类型列表
+	 * @param actorStatues
+	 *            状态列表
+	 * @return 返回结果中的元素Map格式为：
+	 *         <ul>
+	 *         <li>id -- Actor的id</li>
+	 *         <li>type -- Actor的type</li>
+	 *         <li>code -- Actor的code</li>
+	 *         <li>name -- Actor的name</li>
+	 *         <li>pcode -- Actor的pcode</li>
+	 *         <li>pname -- Actor的pname</li>
+	 *         </ul>
+	 */
+	List<Map<String, String>> find4option(Integer[] actorTypes,
+			Integer[] actorStatues);
 }
