e14d9719260cb8c51fa37cbcfe1a451efbb444b9
rongjihuang <rongjihuang@gmail.com>
2011-12-26 00:34:37 +0800

    复制附件代码的实现

--- bc-docs/src/main/java/cn/bc/docs/service/AttachService.java
+++ bc-docs/src/main/java/cn/bc/docs/service/AttachService.java
@@ -36,8 +36,10 @@ public interface AttachService extends CrudService<Attach> {
 	Attach loadByPtype(String ptype, String puid);
 
 	/**
-	 * 复制附件的处理
+	 * 复制附件的处理:仅支持复制app.data.realPath目录下的附件
 	 * 
+	 * @param dataPath
+	 *            全局配置的app.data.realPath目录
 	 * @param fromPtype
 	 *            原附件的ptype
 	 * @param fromPuid
@@ -46,8 +48,10 @@ public interface AttachService extends CrudService<Attach> {
 	 *            新附件的ptype
 	 * @param toPuid
 	 *            新附件的puid
+	 * @param keepAuthorInfo
+	 *            是否保留源附件的创建信息不变，设为false将创建时间和创建人设为当前用户的信息
 	 * @return 复制出来的附件列表
 	 */
-	List<Attach> doCopy(String fromPtype, String fromPuid, String toPtype,
-			String toPuid);
+	List<Attach> doCopy(String dataPath, String fromPtype, String fromPuid,
+			String toPtype, String toPuid, boolean keepAuthorInfo);
 }
