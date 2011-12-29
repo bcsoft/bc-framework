cf5182fe42e7b166ad79e7988663a9a62c6ad7ea
rongjihuang <rongjihuang@gmail.com>
2011-12-29 23:47:40 +0800

    系统日志视图性能优化

--- bc-log/src/main/java/cn/bc/log/web/struts2/SyslogAction.java
+++ bc-log/src/main/java/cn/bc/log/web/struts2/SyslogAction.java
@@ -6,9 +6,6 @@ package cn.bc.log.web.struts2;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,29 +16,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 
-import cn.bc.core.query.condition.Condition;
-import cn.bc.core.query.condition.Direction;
-import cn.bc.core.query.condition.impl.EqualsCondition;
-import cn.bc.core.query.condition.impl.OrderCondition;
 import cn.bc.identity.domain.ActorHistory;
-import cn.bc.identity.web.SystemContext;
 import cn.bc.log.domain.Syslog;
 import cn.bc.log.service.SyslogService;
-import cn.bc.web.formater.CalendarFormater;
 import cn.bc.web.struts2.EntityAction;
-import cn.bc.web.ui.html.grid.Column;
-import cn.bc.web.ui.html.grid.GridData;
-import cn.bc.web.ui.html.grid.TextColumn;
-import cn.bc.web.ui.html.page.PageOption;
-import cn.bc.web.ui.html.toolbar.Toolbar;
-import cn.bc.web.ui.html.toolbar.ToolbarButton;
-import cn.bc.web.ui.html.toolbar.ToolbarSearchButton;
 import cn.bc.web.util.WebUtils;
 
 /**
- * 系统日志Action
+ * 系统日志表单Action
  * 
  * @author dragon
  * 
@@ -54,120 +37,9 @@ public class SyslogAction extends EntityAction<Long, Syslog> implements
 	private static final long serialVersionUID = 1L;
 	public boolean my = false;
 
-	// private SyslogService syslogService;
-
 	@Autowired
 	public void setSyslogService(SyslogService syslogService) {
-		// this.syslogService = syslogService;
 		this.setCrudService(syslogService);
-	}
-
-	@Override
-	protected GridData buildGridData(List<Column> columns) {
-		return super.buildGridData(columns).setRowLabelExpression("subject");
-	}
-
-	@Override
-	protected OrderCondition getDefaultOrderCondition() {
-		return new OrderCondition("fileDate", Direction.Desc);
-	}
-
-	@Override
-	protected Condition getSpecalCondition() {
-		if (!my) {// 查看所有用户的日志信息
-			return null;
-		} else {// 仅查看自己的日志信息
-			ActorHistory curUser = (ActorHistory) ((SystemContext) this.getContext())
-					.getUserHistory();
-			return new EqualsCondition("author.id", curUser.getId());
-		}
-	}
-
-	// 设置页面的尺寸
-	@Override
-	protected PageOption buildListPageOption() {
-		return super.buildListPageOption().setWidth(800).setMinWidth(300)
-				.setHeight(600).setMinHeight(300);
-	}
-
-	@Override
-	protected Toolbar buildToolbar() {
-		Toolbar tb = new Toolbar();
-
-		// 查看按钮
-		tb.addButton(new ToolbarButton().setIcon("ui-icon-check")
-				.setText(getText("label.check"))
-				.setClick("bc.syslogList.checkWork"));
-
-		// 搜索按钮
-		ToolbarSearchButton sb = new ToolbarSearchButton();
-		sb.setAction("search").setTitle(getText("title.click2search"));
-		tb.addButton(sb);
-
-		return tb;
-	}
-
-	@Override
-	protected String[] getSearchFields() {
-		return new String[] { "subject", "author.name", "author.upperName", "author.unitName",
-				"clientIp", "clientInfo", "serverIp" };
-	}
-
-	@Override
-	protected List<Column> buildGridColumns() {
-		List<Column> columns = super.buildGridColumns();
-		columns.add(new TextColumn("fileDate", getText("syslog.createDate"),
-				150).setSortable(true).setDir(Direction.Desc)
-				.setValueFormater(new CalendarFormater("yyyy-MM-dd HH:mm:ss")));
-		columns.add(new TextColumn("type", getText("syslog.type"), 80)
-				.setSortable(true).setValueFormater(
-						new SyslogTypeFormater(getEntityStatuses())));
-		columns.add(new TextColumn("clientIp", getText("syslog.clientIp"), 120)
-				.setSortable(true));
-		columns.add(new TextColumn("author.name", getText("syslog.userName"), 80)
-				.setSortable(true));
-		columns.add(new TextColumn("author.unitName", getText("syslog.unitName"), 100)
-				.setSortable(true));
-		columns.add(new TextColumn("author.upperName", getText("syslog.departName"),
-				100).setSortable(true));
-		if (!my)
-			columns.add(new TextColumn("serverIp", getText("syslog.serverIp"),
-					120).setSortable(true));
-		columns.add(new TextColumn("clientInfo", getText("syslog.clientInfo"))
-				.setSortable(true).setUseTitleFromLabel(true));
-		return columns;
-	}
-
-	@Override
-	protected String getJs() {
-		return contextPath + "/bc/log/syslog/list.js";
-	}
-
-	/**
-	 * 获取系统日志分类值转换列表
-	 * 
-	 * @return
-	 */
-	protected Map<String, String> getEntityStatuses() {
-		Map<String, String> types = new HashMap<String, String>();
-		types = new HashMap<String, String>();
-		types.put(String.valueOf(Syslog.TYPE_LOGIN),
-				getText("syslog.type.login"));
-		types.put(String.valueOf(Syslog.TYPE_LOGOUT),
-				getText("syslog.type.logout"));
-		types.put(String.valueOf(Syslog.TYPE_LOGOUT2),
-				getText("syslog.type.logout2"));
-		return types;
-	}
-
-	@Override
-	protected String getPageTitle() {
-		if (my)
-			return this.getText(StringUtils.uncapitalize(getEntityConfigName())
-					+ ".title.my");
-		else
-			return this.getText(StringUtils.uncapitalize(getEntityConfigName())
-					+ ".title");
 	}
 
 	// 记录登陆日志
