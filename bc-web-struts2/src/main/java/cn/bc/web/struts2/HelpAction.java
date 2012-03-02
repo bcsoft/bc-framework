08c4447fa59dd6eb4285903d5265451c093297f7
rongjihuang <rongjihuang@gmail.com>
2012-03-02 10:24:25 +0800

    增加帮助action

---
+++ bc-web-struts2/src/main/java/cn/bc/web/struts2/HelpAction.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package cn.bc.web.struts2;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 帮助Action
+ * 
+ * @author dragon
+ * 
+ */
+@Controller
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class HelpAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	//private static Log logger = LogFactory.getLog(HelpAction.class);
+	public String f;// 帮助文件的全子路径（不含文件扩展名，如“”）
+
+	public String execute() throws Exception {
+		if(f != null && f.length() > 0){
+			return "file";
+		}else{
+			return "anchor";
+		}
+	}
+}
