c94a1991fb2efeb590127daa6ab5cf02ae068b61
rongjihuang <rongjihuang@gmail.com>
2012-04-25 09:31:43 +0800

    冲突修正

--- bc-template/src/test/java/cn/bc/template/word/docx/DocumentPropertiesTest.java
+++ bc-template/src/test/java/cn/bc/template/word/docx/DocumentPropertiesTest.java
@@ -1,42 +1,32 @@
-package cn.bc.template.word;
+package cn.bc.template.word.docx;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.Assert;
 
-import org.apache.poi.POIXMLProperties;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackagePart;
 import org.apache.poi.openxml4j.opc.PackageProperties;
 import org.apache.poi.openxml4j.opc.PackageRelationship;
 import org.apache.poi.openxml4j.opc.PackageRelationshipTypes;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.PositionInParagraph;
-import org.apache.poi.xwpf.usermodel.TextSegement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.junit.Test;
-import org.openxmlformats.schemas.officeDocument.x2006.customProperties.CTProperties;
-import org.openxmlformats.schemas.officeDocument.x2006.customProperties.CTProperty;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
 import org.springframework.core.io.ClassPathResource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class WordTempalteByPropertiesTest {
+public class DocumentPropertiesTest {
 	// 加载docx文档
 	private OPCPackage getPackage() throws IOException, InvalidFormatException {
 		InputStream is = new ClassPathResource(
-				"cn/bc/template/word/WordTplByProperties.docx")
+				"cn/bc/template/word/docxTpl.docx")
 				.getInputStream();
 		OPCPackage p = OPCPackage.open(is);
 		return p;
@@ -184,14 +174,6 @@ public class WordTempalteByPropertiesTest {
 		inStream.close();
 	}
 
-	// 获取文档的字符串内容
-	@Test
-	public void testGetDocumentText() throws Exception {
-		OPCPackage p = getPackage();
-		XWPFWordExtractor docx = new XWPFWordExtractor(p);
-		System.out.println(docx.getText());
-	}
-
 	// 创建文档
 	@Test
 	public void testCreateDocument() throws Exception {
@@ -213,69 +195,10 @@ public class WordTempalteByPropertiesTest {
 
 		// 生成新的文档
 		XWPFDocument newDoc = new XWPFDocument(p);
-		newDoc.write(new FileOutputStream("d:\\t\\newDoc.docx"));
+		newDoc.write(new FileOutputStream("/t/newDoc_properties.docx"));
 
 		System.out.println("Creator=" + pp.getCreatorProperty().getValue()
 				+ ",Title=" + pp.getTitleProperty().getValue() + ",Subject="
 				+ pp.getSubjectProperty().getValue());
-	}
-
-	// 创建文档
-	@Test
-	public void testGetDocumentAllProperties() throws Exception {
-		OPCPackage p = getPackage();
-
-		// Get core properties part relationship
-		PackageRelationship relationship = p.getRelationshipsByType(
-				PackageRelationshipTypes.CORE_PROPERTIES).getRelationship(0);
-
-		// Get core properties part from the previous relationship
-		PackagePart part = p.getPart(relationship);
-		Assert.assertEquals(
-				"application/vnd.openxmlformats-package.core-properties+xml",
-				part.getContentType());
-		Assert.assertEquals("/docProps/core.xml", part.getPartName().getName());
-
-		XWPFDocument docx = new XWPFDocument(p);
-
-		POIXMLProperties pp = docx.getProperties();
-		CTProperties ctps = pp.getCustomProperties().getUnderlyingProperties();
-//		System.out.println(ctps);
-//		for (CTProperty ctp : ctps.getPropertyList()) {
-//			System.out.println(ctp);
-//			System.out.println(ctp.getLpwstr());
-//			System.out.println(ctp.getI4());
-//		}
-
-		// 设置新的标题：ok
-		pp.getCoreProperties().setTitle("新的标题属性值");
-
-		// 设置新的自定义属性值：ok(需要在Word中配置"打印前更新域"<--选项/显示)
-		// 如果使用后台转换为pdf打印可能存在域没有更新的情况
-		ctps.getPropertyList().get(0).setLpwstr("新的自定义属性值");
-
-		// 替换${name}占位符
-		this.changeText(docx);
-
-		// 写入到新文件
-		docx.write(new FileOutputStream("d:\\t\\newDoc.docx"));
-	}
-
-	// 替换${name}占位符
-	private void changeText(XWPFDocument document) {
-		Iterator<XWPFParagraph> i = document.getParagraphsIterator();
-		while (i.hasNext()) {
-			XWPFParagraph paragraph = i.next();
-			for (XWPFRun run : paragraph.getRuns()) {
-				// System.out.println("========" + run.getCTR());
-				for (CTText t : run.getCTR().getTList()) {
-					if (t.getStringValue().indexOf("${name}") != -1) {
-						//System.out.println("========" + t.getStringValue());
-						t.setStringValue(t.getStringValue().replaceAll(
-								"\\$\\{name\\}", "名称名称"));
-					}
-				}
-			}
-		}
 	}
 }
