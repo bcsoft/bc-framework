bce43105e4be1aeb384538bcd39304ea935538a6
zxr <991316914@qq.com>
2012-04-26 13:37:58 +0800

    平台常数定义添加草稿

--- bc-core/src/main/java/cn/bc/BCConstants.java
+++ bc-core/src/main/java/cn/bc/BCConstants.java
@@ -10,13 +10,15 @@ public class BCConstants {
 	/** 平台命名空间前缀 */
 	public final static String NAMESPACE = "/bc";
 
+	/** 状态：草稿 */
+	public static final int STATUS_DRAFT = -1;
 	/** 状态：正常 */
 	public static final int STATUS_ENABLED = 0;
 	/** 状态：已禁用 */
 	public static final int STATUS_DISABLED = 1;
 	/** 状态：已删除 */
 	public static final int STATUS_DELETED = 2;
-	
+
 	/** 流水号连接符 */
 	public static final String SN_SPLIT_SYMBOL = "-";
 }
