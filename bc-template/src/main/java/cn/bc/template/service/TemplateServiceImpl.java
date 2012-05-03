6dc747b450dbef3f5af64355b44c0139e4dd1e92
fatfish <fatfish2010@sina.cn>
2012-05-03 18:20:21 +0800

    模板管理接口优化

--- bc-template/src/main/java/cn/bc/template/service/TemplateServiceImpl.java
+++ bc-template/src/main/java/cn/bc/template/service/TemplateServiceImpl.java
@@ -7,13 +7,18 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 import cn.bc.BCConstants;
 import cn.bc.core.service.DefaultCrudService;
 import cn.bc.core.util.TemplateUtils;
 import cn.bc.template.dao.TemplateDao;
 import cn.bc.template.domain.Template;
+import cn.bc.template.util.DocxUtils;
+import cn.bc.template.util.XlsUtils;
 
 /**
  * Service接口的实现
@@ -47,8 +52,18 @@ public class TemplateServiceImpl extends DefaultCrudService<Template> implements
 			logger.warn("没有找到编码为'" + code + "'的模板!");
 			return null;
 		}
-
-		return tpl.getContent();
+		//纯文本类型
+		if(tpl.isPureText())return tpl.getContent();
+		// 附件的扩展名
+		String extension = StringUtils.getFilenameExtension(tpl.getPath());
+		if(tpl.getType()==Template.TYPE_WORD&&extension.equals("docx"))
+			return DocxUtils.loadText(tpl.getInputStream());
+		
+		if(tpl.getType()==Template.TYPE_EXCEL&&extension.equals("xls"))
+			return XlsUtils.loadText(tpl.getInputStream());
+			
+		logger.warn("文件后缀名："+extension+",未转换为字符串类型");
+		return null;
 	}
 
 	public InputStream getInputStream(String code) {
@@ -57,7 +72,7 @@ public class TemplateServiceImpl extends DefaultCrudService<Template> implements
 			logger.warn("没有找到编码为'" + code + "'的模板!");
 			return null;
 		}
-
+		
 		return tpl.getInputStream();
 	}
 
@@ -67,13 +82,42 @@ public class TemplateServiceImpl extends DefaultCrudService<Template> implements
 	}
 
 	public void formatTo(String code, Map<String, Object> args, OutputStream out) {
-		String source = this.getContent(code);
-		String r = TemplateUtils.format(source, args);
-		try {
-			out.write(r.getBytes());
-			out.close();
-		} catch (IOException e) {
-			logger.warn("formatTo 写入数据到流错误：" + e.getMessage());
+		Template tpl = loadByCode(code);
+		if (tpl == null) logger.warn("没有找到编码为'" + code + "'的模板!");
+		//纯文本类型
+		if(tpl.isPureText()){
+			String source = this.getContent(code);
+			String r = TemplateUtils.format(source, args);
+			try {
+				out.write(r.getBytes());
+				out.close();
+			} catch (IOException e) {
+				logger.warn("formatTo 写入数据到流错误：" + e.getMessage());
+			}
+		}else{
+			// 附件的扩展名
+			String extension = StringUtils.getFilenameExtension(tpl.getPath()); 
+			
+			if(tpl.getType()==Template.TYPE_WORD&&extension.equals("docx")){
+				XWPFDocument docx=DocxUtils.format(tpl.getInputStream(), args);
+				try {
+					docx.write(out);
+					out.close();
+				} catch (IOException e) {
+					logger.warn("formatTo 写入数据到流错误：" + e.getMessage());
+				}
+			}else if(tpl.getType()==Template.TYPE_EXCEL&&extension.equals("xls")){
+				HSSFWorkbook xls=XlsUtils.format(tpl.getInputStream(), args);
+				try {
+					xls.write(out);
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					logger.warn("formatTo 写入数据到流错误：" + e.getMessage());
+				}
+			}else{
+				logger.warn("文件后缀名："+extension+",不能formatTo");
+			}
 		}
 	}
 
