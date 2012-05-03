5d7a40aeb4bba29c1e776755926d173df2c87830
fatfish <fatfish2010@sina.cn>
2012-05-03 18:21:54 +0800

    模板管理根据配置参数实现在线预览功能

--- bc-template/src/main/java/cn/bc/template/web/struts2/TemplateAction.java
+++ bc-template/src/main/java/cn/bc/template/web/struts2/TemplateAction.java
@@ -1,21 +1,48 @@
 package cn.bc.template.web.struts2;
 
-import java.util.ArrayList;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.struts2.ServletActionContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 
 import cn.bc.BCConstants;
+import cn.bc.core.util.DateUtils;
+import cn.bc.core.util.TemplateUtils;
+import cn.bc.docs.web.AttachUtils;
 import cn.bc.identity.web.SystemContext;
 import cn.bc.identity.web.struts2.FileEntityAction;
 import cn.bc.template.domain.Template;
 import cn.bc.template.service.TemplateService;
+import cn.bc.template.util.DocxUtils;
+import cn.bc.template.util.FreeMarkerUtils;
+import cn.bc.template.util.XlsUtils;
 import cn.bc.web.ui.html.page.ButtonOption;
 import cn.bc.web.ui.html.page.PageOption;
 import cn.bc.web.ui.json.Json;
+import cn.bc.web.util.WebUtils;
+
+import com.artofsolving.jodconverter.DefaultDocumentFormatRegistry;
+import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.DocumentFormatRegistry;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
 
 /**
  * 模板表单Action
@@ -121,71 +148,187 @@ public class TemplateAction extends FileEntityAction<Long, Template> {
 	//---- 加载配置参数  ---开始--
 	public String loadTplConfigParam(){
 		Json json = new Json();
+		// 附件的扩展名
+		String extension = StringUtils.getFilenameExtension(path); 
+		
 		//自定义文本
 		if(type.equals(Template.TYPE_CUSTOM)){
-			//保存参数的字符串
-			String param=this.findParam(content);
-			if(param==null){
-				this.json=json.toString();
-				return "json";
-			}
-			json.put("value", param);
+			//保存参数的集合	
+			json.put("value", this.getParamStr(TemplateUtils.findMarkers(content)));
+		}else if(type.equals(Template.TYPE_EXCEL)&&extension.equals("xls")){
+			Template excel=new Template();
+			excel.setPath(this.path);
+			json.put("value",this.getParamStr(XlsUtils.findMarkers(excel.getInputStream())));
+		}else if(type.equals(Template.TYPE_WORD)&&extension.equals("docx")){
+			Template word=new Template();
+			word.setPath(this.path);
+			json.put("value",this.getParamStr(DocxUtils.findMarkers(word.getInputStream())));
+		}else if(type.equals(Template.TYPE_TEXT)){
+			Template txt=new Template();
+			txt.setPath(this.path);
+			json.put("value",this.getParamStr(TemplateUtils.findMarkers(txt.getInputStream())));
 		}
-		
 		this.json=json.toString();
 		return "json";
 	}
 	
-	/**查找字符串中的参数
-	 * 
-	 * @param 字符串text
-	 * @return 参数1,参数2,参数3的字符串
-	 */
-	private String findParam(String text){
-		if(text==null)
-			return null;
-		//临时保存参数的集合
-		List<String> tempList=new ArrayList<String>();
-		
-		String[] textArr=text.split("\\$\\{");
-		for(int i=0;i<textArr.length;i++){
-			if(i>0){
-				String tempParam="";
-				//检测此字符串是否带有"}"
-				int indx=textArr[i].indexOf("}");
-				if(indx<0)//没
-					return null;
-				if(indx>0)
-					tempParam=textArr[i].substring(0,indx);
-				if(tempList.isEmpty()){
-					tempList.add(tempParam);
-				}else{
-					boolean equal=false;
-					for(String str:tempList){
-						if(str.equals(tempParam)){
-							equal=true;
-							break;
-						}
-					}
-					//不相同的加入集合
-					if(!equal){
-						tempList.add(tempParam);
-					}
-				}
+	private String getParamStr(List<String> list){
+		String param=null;
+		for(int i=0;i<list.size();i++){
+			if(i==0){
+				param=list.get(i);
+			}else{
+				param= param+","+list.get(i);	
 			}
+			
 		}
-		
-		//保存参数的字符串
-		String param="";
-		for(int i=0;i<tempList.size();i++){
-			param+=tempList.get(i);
-			if((i+1)!=tempList.size()){
-				param+=",";
-			}
-		}
-		if(param.length()>0)
-			return param; 
-		return null;
+		return param;
 	}
+	
 	//---- 加载配置参数  ---结束--
+	
+	public String filename;
+	public String contentType;
+	public long contentLength;
+	public InputStream inputStream;
+	
+	//下载自定义文本
+	public String download() throws Exception {
+		Template coustText=this.templateService.load(tid);
+		Date startTime = new Date();
+		// 附件的扩展名
+		String extension = "txt";
+		// debug
+		if (logger.isDebugEnabled()) {
+			logger.debug("extension=" + extension);
+		}
+		// 设置下载文件的参数
+		this.contentType = AttachUtils.getContentType(extension);
+		this.filename = WebUtils.encodeFileName(
+				ServletActionContext.getRequest(), coustText.getSubject()+"."+extension);
+		this.contentLength = coustText.getContent().length();
+		this.inputStream = coustText.getInputStream();
+		if (logger.isDebugEnabled()) {
+			logger.debug("download:" + DateUtils.getWasteTime(startTime));
+		}
+		return SUCCESS;
+	}
+	
+	
+	private static final int BUFFER = 4096;
+	public String from;// 指定原始文件的类型，默认为文件扩展名
+	public String to;// 预览时转换到的文件类型，默认为pdf
+	public String f;// 要下载的文件，相对于Attach.DATA_REAL_PATH下的子路径，前后均不带/
+	public String n;// [可选]指定下载为的文件名
+	public String markerValueJsons;
+	
+	//在线查看
+	public String inline() throws Exception {
+		Template template=this.templateService.load(tid);		
+		Date startTime = new Date();
+		
+		// 附件的扩展名
+		String extension = StringUtils.getFilenameExtension(template.getPath()); 
+		
+		// debug
+		if (logger.isDebugEnabled()) {
+			logger.debug("path=" + template.getPath());
+			logger.debug("extension=" + extension);
+			logger.debug("n=" + template.getSubject());
+			logger.debug("to=" + to);
+		}
+		
+		this.n = StringUtils.getFilename(template.getSubject());
+		if (isConvertFile(extension)||template.isPureText()) {
+			//解释需要配置参数替换为指定的值。
+			Map<String, Object> markerValues=new HashMap<String, Object>();
+			JSONArray jsons = new JSONArray(this.markerValueJsons);
+			JSONObject json;
+			for (int i = 0; i < jsons.length(); i++) {
+				json = jsons.getJSONObject(i);
+				markerValues.put(json.getString("key"), json.get("value"));
+			}		
+			InputStream inputStream=null;
+			if(template.getType()==Template.TYPE_WORD&&extension.equals("docx")){
+				XWPFDocument docx=DocxUtils.format(template.getInputStream(), markerValues);
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				docx.write(out);
+				inputStream=new ByteArrayInputStream(out.toByteArray());
+			}else if(template.getType()==Template.TYPE_EXCEL&&extension.equals("xls")){
+				HSSFWorkbook xls=XlsUtils.format(template.getInputStream(), markerValues);
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				xls.write(out);
+				inputStream=new ByteArrayInputStream(out.toByteArray());
+			}else if(template.isPureText()){
+				template.setContent(FreeMarkerUtils.format(template.getContent(), markerValues));
+				inputStream=template.getInputStream();
+			}else{
+				inputStream =template.getInputStream();
+			}
+			
+			// 调用jodconvert将附件转换为pdf文档后再下载
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(
+					BUFFER);
+
+			// connect to an OpenOffice.org instance running on port 8100
+			OpenOfficeConnection connection = new SocketOpenOfficeConnection(
+					getText("jodconverter.soffice.host"),
+					Integer.parseInt(getText("jodconverter.soffice.port")));
+			connection.connect();
+			if (logger.isDebugEnabled()) {
+				logger.debug("connect:" + DateUtils.getWasteTime(startTime));
+			}
+
+			DocumentFormatRegistry formaters = new DefaultDocumentFormatRegistry();
+
+			// convert
+			DocumentConverter converter = new OpenOfficeDocumentConverter(
+					connection);
+			if (this.from == null || this.from.length() == 0)
+				this.from = extension;
+			if (this.to == null || this.to.length() == 0)
+				this.to = getText("jodconverter.to.extension");// 没有指定就是用系统默认的配置转换为pdf
+			converter.convert(inputStream,
+					formaters.getFormatByFileExtension(this.from), outputStream,
+					formaters.getFormatByFileExtension(this.to));
+			if (logger.isDebugEnabled()) {
+				logger.debug("convert:" + DateUtils.getWasteTime(startTime));
+			}
+
+			// close the connection
+			connection.disconnect();
+
+			// 设置下载文件的参数（设置不对的话，浏览器是不会直接打开的）
+			byte[] bs = outputStream.toByteArray();
+			this.inputStream = new ByteArrayInputStream(bs);
+			this.inputStream.close();
+			this.contentType = AttachUtils.getContentType(this.to);
+			this.contentLength = bs.length;
+			this.filename = WebUtils.encodeFileName(ServletActionContext
+					.getRequest(), this.n == null ? "bc" : this.n + "."
+					+ this.to);
+		} else {
+			// 设置下载文件的参数
+			this.filename = WebUtils.encodeFileName(
+					ServletActionContext.getRequest(), this.n);
+			this.contentType = AttachUtils.getContentType(extension);
+			// 无需转换的文档直接下载处理
+			File file = new File(template.getPath());
+			this.contentLength = file.length();
+			this.inputStream = new FileInputStream(file);	
+		}
+
+		return SUCCESS;
+	}
+
+	// 判断指定的扩展名是否为配置的要转换的文件类型
+	private boolean isConvertFile(String extension) {
+		String[] extensions = getText("jodconverter.from.extensions")
+				.split(",");
+		for (String ext : extensions) {
+			if (ext.equalsIgnoreCase(extension))
+				return true;
+		}
+		return false;
+	}
 }
