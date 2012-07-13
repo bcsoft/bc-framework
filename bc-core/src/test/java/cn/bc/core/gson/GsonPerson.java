3b793ece094c1b548d094a9450337eac76174b4e
dragon <rongjihuang@gmail.com>
2012-07-13 16:02:33 +0800

    增加使用Gson的Json处理工具方法

---
+++ bc-core/src/test/java/cn/bc/core/gson/GsonPerson.java
@@ -0,0 +1,38 @@
+package cn.bc.core.gson;
+
+/**
+ * Gson测试用
+ * 
+ * @author dragon
+ * 
+ */
+public class GsonPerson {
+	private String name;
+	private int age;
+
+	public GsonPerson(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return name + ":" + age;
+	}
+}
---
+++ bc-core/src/test/java/cn/bc/core/util/JsonUtilsTest.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ */
+package cn.bc.core.util;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import cn.bc.core.gson.GsonPerson;
+
+import com.google.gson.JsonParseException;
+
+/**
+ * 
+ * @author dragon
+ * 
+ */
+public class JsonUtilsTest {
+	@Test
+	public void toJson_Object() {
+		GsonPerson obj = new GsonPerson("name1", 1);
+		Assert.assertEquals("{\"name\":\"name1\",\"age\":1}",
+				JsonUtils.toJson(obj));
+	}
+
+	@Test
+	public void toJson_String() {
+		Assert.assertEquals("\"name\"", JsonUtils.toJson("name"));
+	}
+
+	@Test(expected = JsonParseException.class)
+	public void toMap_Exception_String() {
+		JsonUtils.toMap("name");
+	}
+
+	@Test(expected = JsonParseException.class)
+	public void toMap_Exception_Array() {
+		JsonUtils.toMap("[\"name\"]");
+	}
+
+	@Test
+	public void toMap() {
+		String str = "{\"str\":\"str\",\"int\":1,\"bool\":true}";
+		Map<String, Object> map = JsonUtils.toMap(str);
+		Assert.assertEquals("str", map.get("str"));
+		Assert.assertEquals(String.class, map.get("str").getClass());
+		Assert.assertEquals(new Integer(1), map.get("int"));
+		Assert.assertEquals(Integer.class, map.get("int").getClass());
+		Assert.assertEquals(new Boolean(true), map.get("bool"));
+		Assert.assertEquals(Boolean.class, map.get("bool").getClass());
+	}
+
+	@Test
+	public void toMap_NestedMap() {
+		String str = "{\"str\":\"str\",\"int\":1,\"bool\":true,\"sub\":{\"str\":\"str\",\"int\":1,\"bool\":true}}";
+		Map<String, Object> map = JsonUtils.toMap(str);
+
+		Assert.assertEquals("str", map.get("str"));
+		Assert.assertEquals(String.class, map.get("str").getClass());
+		Assert.assertEquals(new Integer(1), map.get("int"));
+		Assert.assertEquals(Integer.class, map.get("int").getClass());
+		Assert.assertEquals(new Boolean(true), map.get("bool"));
+		Assert.assertEquals(Boolean.class, map.get("bool").getClass());
+
+		Assert.assertTrue(map.get("sub") instanceof Map);
+		@SuppressWarnings({ "unchecked" })
+		Map<String, Object> sub = (Map<String, Object>) map.get("sub");
+		Assert.assertEquals("str", sub.get("str"));
+		Assert.assertEquals(String.class, sub.get("str").getClass());
+		Assert.assertEquals(new Integer(1), sub.get("int"));
+		Assert.assertEquals(Integer.class, sub.get("int").getClass());
+		Assert.assertEquals(new Boolean(true), sub.get("bool"));
+		Assert.assertEquals(Boolean.class, sub.get("bool").getClass());
+	}
+
+	@Test
+	public void toMap_Nested_Array_Simple() {
+		String str = "{\"str\":\"str\",\"int\":1,\"bool\":true,\"array\":[\"a1\",1,true]}";
+		Map<String, Object> map = JsonUtils.toMap(str);
+
+		Assert.assertEquals("str", map.get("str"));
+		Assert.assertEquals(String.class, map.get("str").getClass());
+		Assert.assertEquals(new Integer(1), map.get("int"));
+		Assert.assertEquals(Integer.class, map.get("int").getClass());
+		Assert.assertEquals(new Boolean(true), map.get("bool"));
+		Assert.assertEquals(Boolean.class, map.get("bool").getClass());
+
+		Assert.assertEquals(Object[].class, map.get("array").getClass());
+		Object[] array = (Object[]) map.get("array");
+		Assert.assertEquals(String.class, array[0].getClass());
+		Assert.assertEquals("a1", array[0]);
+		Assert.assertEquals(Integer.class, array[1].getClass());
+		Assert.assertEquals(1, array[1]);
+		Assert.assertEquals(Boolean.class, array[2].getClass());
+		Assert.assertEquals(new Boolean(true), array[2]);
+	}
+
+	@Test
+	public void toArray() {
+		String str = "[\"a1\",1,true]";
+		Object[] array = JsonUtils.toArray(str);
+		Assert.assertEquals(String.class, array[0].getClass());
+		Assert.assertEquals("a1", array[0]);
+		Assert.assertEquals(Integer.class, array[1].getClass());
+		Assert.assertEquals(1, array[1]);
+		Assert.assertEquals(Boolean.class, array[2].getClass());
+		Assert.assertEquals(new Boolean(true), array[2]);
+	}
+
+	@Test
+	public void toArray_NestedMap() {
+		String str = "[{\"str\":\"str\",\"int\":1,\"bool\":true}]";
+		Object[] array = JsonUtils.toArray(str);
+		Assert.assertEquals(LinkedHashMap.class, array[0].getClass());
+		@SuppressWarnings("unchecked")
+		Map<String, Object> sub = (Map<String, Object>) array[0];
+		Assert.assertEquals("str", sub.get("str"));
+		Assert.assertEquals(String.class, sub.get("str").getClass());
+		Assert.assertEquals(new Integer(1), sub.get("int"));
+		Assert.assertEquals(Integer.class, sub.get("int").getClass());
+		Assert.assertEquals(new Boolean(true), sub.get("bool"));
+		Assert.assertEquals(Boolean.class, sub.get("bool").getClass());
+	}
+
+	@Test
+	public void toCollection() {
+		String str = "[\"a1\",1,true]";
+		Collection<Object> col = JsonUtils.toCollection(str);
+		// System.out.println(col.getClass());//java.util.LinkedList
+		Object[] array = col.toArray();
+		Assert.assertEquals(String.class, array[0].getClass());
+		Assert.assertEquals("a1", array[0]);
+		Assert.assertEquals(Integer.class, array[1].getClass());
+		Assert.assertEquals(1, array[1]);
+		Assert.assertEquals(Boolean.class, array[2].getClass());
+		Assert.assertEquals(new Boolean(true), array[2]);
+	}
+}
