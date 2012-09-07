8ab27b1f22c63697cbae79f1d18c3caab531c0d6
dragon <rongjihuang@gmail.com>
2012-09-07 14:38:34 +0800

    增加Mustache模板的引用

---
+++ bc-template/src/test/java/cn/bc/template/util/MustacheTest.java
@@ -0,0 +1,38 @@
+package cn.bc.template.util;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.Writer;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+public class MustacheTest {
+
+	@Test
+	public void test() throws IOException {
+		HashMap<String, Object> scopes = new HashMap<String, Object>();
+		scopes.put("name", "Mustache");
+		scopes.put("feature", new Feature("Perfect!"));
+
+		Writer writer = new OutputStreamWriter(System.out);
+		MustacheFactory mf = new DefaultMustacheFactory();
+		Mustache mustache = mf.compile(new StringReader(
+				"{{name}}, {{feature.description}}!"), "example");
+		mustache.execute(writer, scopes);
+		writer.flush();
+	}
+
+	class Feature {
+		String description;
+
+		public Feature(String description) {
+			this.description = description;
+		}
+	}
+}
