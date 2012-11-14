1bb01acbb038e7339be590df7d8c0eda54525318
dragon <rongjihuang@gmail.com>
2012-11-14 10:45:25 +0800

    树的初始封装

---
+++ bc-web/src/main/java/cn/bc/web/ui/html/tree/Tree.java
@@ -0,0 +1,108 @@
+package cn.bc.web.ui.html.tree;
+
+import cn.bc.web.ui.json.Json;
+
+/**
+ * 树(根节点)
+ * 
+ * @author dragon
+ * 
+ */
+public class Tree extends TreeNode {
+	private String url; // 获取树数据的url
+	private Json cfg; // 配置参数
+	private boolean showRoot; // 是否显示根节点
+	private boolean selectable; // 鼠标是否可以选择树的文字
+
+	public Tree() {
+		this(null, null);
+	}
+
+	/**
+	 * @param rootNodeId
+	 *            根节点的节点ID值
+	 * @param rootNodeLabel
+	 *            根节点名称
+	 */
+	public Tree(String rootNodeId, String rootNodeLabel) {
+		super(rootNodeId, rootNodeLabel);
+		this.setOpen(true);
+		this.setLeaf(false);
+	}
+
+	@Override
+	public String getTag() {
+		return "div";
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public Tree setUrl(String url) {
+		this.url = url;
+		return this;
+	}
+
+	public Json getCfg() {
+		return cfg;
+	}
+
+	public Tree setCfg(Json cfg) {
+		this.cfg = cfg;
+		return this;
+	}
+
+	public boolean isShowRoot() {
+		return showRoot;
+	}
+
+	public Tree setShowRoot(boolean showRoot) {
+		this.showRoot = showRoot;
+		return this;
+	}
+
+	public boolean isSelectable() {
+		return selectable;
+	}
+
+	public Tree setSelectable(boolean selectable) {
+		this.selectable = selectable;
+		return this;
+	}
+
+	@Override
+	protected void init() {
+		this.addClazz("bc-tree ui-widget-content treeNode");
+	}
+
+	@Override
+	public StringBuffer render(StringBuffer main) {
+		// 树属性的渲染
+		this.setAttr("data-url", this.getUrl());
+		if (this.getCfg() != null)
+			this.setAttr("data-cfg", this.getCfg().toString());
+		if (!this.isSelectable())
+			this.addClazz("unselectable");
+
+		// 执行基类的渲染
+		return super.render(main);
+	}
+
+	@Override
+	protected void renderItem(StringBuffer main) {
+		if (isShowRoot()) {
+			super.renderItem(main);
+		}
+	}
+
+	@Override
+	protected void renderClass() {
+		super.renderClass();
+	}
+
+	@Override
+	protected void renderSubNodes(StringBuffer main) {
+		super.renderSubNodes(main);
+	}
+}
---
+++ bc-web/src/test/java/cn/bc/web/ui/tree/TreeTest.java
@@ -0,0 +1,69 @@
+package cn.bc.web.ui.tree;
+
+import org.junit.Test;
+
+import cn.bc.web.ui.html.tree.Tree;
+import cn.bc.web.ui.html.tree.TreeNode;
+
+public class TreeTest {
+	@Test
+	public void test01_treeNode_leaf() {
+		TreeNode node = new TreeNode("1", "叶子节点");
+		node.setLeaf(true);
+		System.out.println(node);
+	}
+
+	@Test
+	public void test02_treeNode_folder_open() {
+		TreeNode node = new TreeNode("2", "父节点(展开状态)");
+		node.setLeaf(false);
+		node.setOpen(true);
+		System.out.println(node);
+	}
+
+	@Test
+	public void test03_treeNode_folder_collapsed() {
+		TreeNode node = new TreeNode("3", "父节点(折叠状态)");
+		node.setLeaf(false);
+		node.setOpen(false);
+		System.out.println(node);
+	}
+
+	@Test
+	public void test04_treeNode_subNodes() {
+		TreeNode node = new TreeNode("4", "父节点(展开状态)-带子节点");
+		node.setLeaf(false);
+		node.setOpen(true);
+		
+		TreeNode subNode = new TreeNode("4-1", "子节点1");
+		subNode.setLeaf(true);
+		node.addSubNode(subNode);
+		
+		subNode = new TreeNode("4-2", "子节点2");
+		subNode.setLeaf(true);
+		subNode.setIcon("sc2");
+		node.addSubNode(subNode);
+		
+		System.out.println(node);
+	}
+
+	@Test
+	public void test04_tree() {
+		Tree tree = new Tree("0", "根节点(默认展开状态)");
+		
+		TreeNode node = new TreeNode("1", "父节点1");
+		node.setLeaf(false);
+		node.setOpen(true);
+		tree.addSubNode(node);
+		
+		TreeNode subNode = new TreeNode("1-1", "子节点11");
+		subNode.setLeaf(true);
+		node.addSubNode(subNode);
+		
+		node = new TreeNode("2", "叶子节点");
+		node.setLeaf(true);
+		tree.addSubNode(node);
+		
+		System.out.println(tree);
+	}
+}
