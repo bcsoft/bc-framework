04092831be25fa60d83b59a814bb6c4bba1c107a
dragon <rongjihuang@gmail.com>
2012-12-11 17:23:30 +0800

    更新触发器的代码，没刷一次卡更新司机的身份证信息和最后修改人信息

--- docs/DataBaseDesign/idcard.sql
+++ docs/DataBaseDesign/idcard.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS BC_IDCARD_CHECK;
 DROP TABLE IF EXISTS BC_IDCARD;
+DROP TABLE IF EXISTS BC_IDCARD_PIC;
 DROP sequence IF EXISTS IDCARD_SEQUENCE;
 
 -- 创建刷卡记录用的序列，开始于1000
@@ -27,7 +28,6 @@ CREATE TABLE BC_IDCARD(
 	MODIFIED_DATE TIMESTAMP,
 	MODIFIER_ID int,
 	ISNEW int NOT NULL default 0,
-	PIC BYTEA,
 	CONSTRAINT BCPK_IDCARD PRIMARY KEY (ID),
 	CONSTRAINT BCUK_IDCARD_NUM UNIQUE (TYPE_,CODE)
 );
@@ -49,12 +49,22 @@ COMMENT ON COLUMN BC_IDCARD.FILE_DATE IS '创建时间';
 COMMENT ON COLUMN BC_IDCARD.AUTHOR_ID IS '创建人:对应Actor的ID';
 COMMENT ON COLUMN BC_IDCARD.MODIFIED_DATE IS '最后更新时间';
 COMMENT ON COLUMN BC_IDCARD.MODIFIER_ID IS '最后更新人:对应Actor的ID';
-COMMENT ON COLUMN BC_IDCARD.PIC IS '图像';
 ALTER TABLE BC_IDCARD ADD CONSTRAINT BCFK_IDCARD_AUTHOR FOREIGN KEY (AUTHOR_ID)
 	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE BC_IDCARD ADD CONSTRAINT BCFK_IDCARD_MODIFIER FOREIGN KEY (MODIFIER_ID)
 	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 CREATE INDEX BCIDX_IDCARD_CODE ON BC_IDCARD (TYPE_, CODE, ISNEW);
+
+-- ALTER TABLE bc_idcard DROP COLUMN pic;
+CREATE TABLE BC_IDCARD_PIC(
+	ID INT NOT NULL,
+	DATA_ BYTEA,
+	CONSTRAINT BCPK_IDCARD_PIC PRIMARY KEY (ID)
+);
+COMMENT ON TABLE BC_IDCARD_PIC IS '身份证图像';
+COMMENT ON COLUMN BC_IDCARD_PIC.DATA_ IS '图像的二进制数据';
+ALTER TABLE BC_IDCARD_PIC ADD CONSTRAINT BCFK_IDCARD_PIC FOREIGN KEY (ID)
+	REFERENCES BC_IDCARD (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 CREATE TABLE BC_IDCARD_CHECK(
 	ID INT NOT NULL,
@@ -87,69 +97,61 @@ insert into BC_IDENTITY_ACTOR_RELATION (TYPE_,MASTER_ID,FOLLOWER_ID)
 	where am.code='IDCARD_USERS' and af.code in ('admin','ghy','xu','hrj','lubaojin','zxr')
 	and not exists (select 0 from BC_IDENTITY_ACTOR_RELATION ar where ar.TYPE_=0 and ar.MASTER_ID=am.id and ar.FOLLOWER_ID=af.id); 
 
--- test
-INSERT INTO bc_idcard(id, code, name, sex, folk, birthdate, issue, start_date, end_date, address, file_date, author_id)
-    VALUES (1, '440721197602067339','dragon', '男', '汉', '1976-02-06', '广州市公安局海珠分局', '2006-05-18', '2026-05-18', 'address',now(),9);
-INSERT INTO bc_idcard_check(id, pid, file_date, author_id)
-    VALUES (1, 1, '2012-11-01 00:00:00',9);
-INSERT INTO bc_idcard_check(id, pid, file_date, author_id)
-    VALUES (2, 1, '2012-12-010 00:00:00',9);
-
 -- 创建触发器
 CREATE or Replace FUNCTION antoInsertTempDriver() RETURNS trigger AS $$  
 DECLARE
-	did int := 0;
+	guy RECORD;
+	driver_id int := 0;	
 BEGIN
-	select id into did from bs_temp_driver where cert_identity=NEW.code;
-	If (did > 0) THEN  
-		If (TG_OP = 'UPDATE') THEN  
-			-- 更新司机招聘信息
-			update bs_temp_driver set name=NEW.name
-				,sex=CASE WHEN NEW.sex='男' THEN 1 ELSE 2 END
-				,nation=NEW.FOLK
-				,birthdate=NEW.BIRTHDATE
-				,address=NEW.ADDRESS
-				,cert_identity=NEW.code
-				,issue=NEW.ISSUE
-				,valid_start_date=NEW.START_DATE
-				,valid_end_date=NEW.END_DATE
-				,file_date=NEW.FILE_DATE
-				,author_id=1146
-				where id=did;
+	FOR guy IN select * from bc_idcard where id=NEW.pid LOOP
+		-- 获取已经存在的司机的id
+		select id into driver_id from bs_temp_driver where cert_identity=guy.code limit 1;
+
+		If (driver_id > 0) THEN  -- 司机已存在：更新身份证信息、最后修改信息
+			update bs_temp_driver set name=guy.name
+				,sex=to_number(guy.sex,'FM999')
+				,nation=guy.FOLK
+				,birthdate=guy.BIRTHDATE
+				,address=guy.ADDRESS
+				,issue=guy.ISSUE
+				,valid_start_date=guy.START_DATE
+				,valid_end_date=guy.END_DATE
+				,register_date=guy.FILE_DATE
+				,modifier_id=(select id from bc_identity_actor_history where current=true and actor_id=guy.author_id)
+				,modified_date=NEW.FILE_DATE
+				where id=driver_id;
 			return null; 
-		END IF;
-	Else 
-		If (TG_OP = 'INSERT') THEN  
-			-- 插入新的司机招聘信息
+		Else   -- 司机不存在：插入新的信息
 			INSERT INTO bs_temp_driver(id,uid_,status_,name
 				,sex,nation,birthdate,address
-				,cert_identity,issue,valid_start_date,valid_end_date,file_date,author_id,region_)
-				VALUES (NEXTVAL('hibernate_sequence'),'tempDriver.auid.'||NEXTVAL('hibernate_sequence'),0,NEW.name
-				,CASE WHEN NEW.sex='男' THEN 1 ELSE 2 END,NEW.FOLK,NEW.BIRTHDATE,NEW.ADDRESS
-				,NEW.code,NEW.ISSUE,NEW.START_DATE,NEW.END_DATE,NEW.FILE_DATE,1146
-				,CASE WHEN NEW.address like '%广州%' THEN 1 
-					WHEN NEW.address like '%广东%' THEN 2 
-					WHEN NEW.CODE like '44%' THEN 2 
+				,cert_identity,issue,valid_start_date,valid_end_date,register_date
+				,file_date,author_id,modified_date,modifier_id,region_)
+				VALUES (NEXTVAL('hibernate_sequence'),'tempDriver.auid.'||NEXTVAL('hibernate_sequence'),0,guy.name
+				,to_number(guy.sex,'FM999'),guy.FOLK,guy.BIRTHDATE,guy.ADDRESS
+				,guy.code,guy.ISSUE,guy.START_DATE,guy.END_DATE,guy.FILE_DATE
+				,guy.FILE_DATE,(select id from bc_identity_actor_history where current=true and actor_id=guy.author_id)
+				,guy.FILE_DATE,(select id from bc_identity_actor_history where current=true and actor_id=guy.author_id)
+				,CASE WHEN guy.address like '%广州%' THEN 1 
+					WHEN guy.address like '%广东%' THEN 2 
+					WHEN guy.CODE like '44%' THEN 2 
 					ELSE 3 END);
 			return null;  
 		END IF;
-	END IF;
+	END LOOP;
 	return null;
 END;
 $$ LANGUAGE plpgsql;  
-DROP TRIGGER IF EXISTS trigger_antoInsertTempDriver on BC_IDCARD;
-CREATE TRIGGER trigger_antoInsertTempDriver AFTER INSERT or UPDATE ON BC_IDCARD  
+DROP TRIGGER IF EXISTS trigger_antoInsertTempDriver on BC_IDCARD_CHECK;
+CREATE TRIGGER trigger_antoInsertTempDriver AFTER INSERT ON BC_IDCARD_CHECK  
     FOR EACH ROW EXECUTE PROCEDURE antoInsertTempDriver();
 
-delete from bs_temp_driver;
+-- test
+INSERT INTO bc_idcard(id, code, name, sex, folk, birthdate, issue, start_date, end_date, address, file_date, author_id)
+    VALUES (1, 'ID01','dragon', '1', '汉族', '1976-02-01', '广州市公安局', '2006-02-01', '2026-02-01', '广州市XXX001',now(),9);
+INSERT INTO bc_idcard_check(id, pid, file_date, author_id)
+    VALUES (NEXTVAL('hibernate_sequence'), 1, now(),9);
 
--- t
-select * from bc_idcard_check;
-select NEXTVAL('IDCARD_SEQUENCE') as ID;
-select to_char(now(),'YYYY-MM-DD HH24:MI:SS.MS') as T from bc_dual;
-
-select * from bc_identity_auth;
-select * from bc_identity_actor_history order by id;
+-- 刷卡器的登录sql
 select * from bc_identity_actor u 
 	inner join bc_identity_auth a on a.id=u.id
 	inner join bc_identity_actor_relation ur on ur.follower_id = u.id and ur.type_=0
@@ -157,8 +159,16 @@ select * from bc_identity_actor u
 	where u.type_=4 and u.code='dragon' and a.password = '21218cca77804d2ba1922c33e0151105'
 	and g.code='IDCARD_USERS';
 
-SELECT b.*,a.* FROM bc_idcard a left join bc_idcard_check b on b.pid=a.id;
-select * from bs_temp_driver;
+-- 清空删除测试数据
+delete from bc_idcard_check;
+delete from bc_idcard_pic;
+delete from bc_idcard;
+delete from bs_temp_driver;
+delete from bs_temp_driver_workflow;
+delete from bs_temp_driver where id=10150592;
+-- update bs_temp_driver set valid_start_date = null;
 
-INSERT INTO bc_idcard(id, code, name, sex, folk, birthdate, issue, start_date, end_date, address, file_date, author_id)
-    VALUES (NEXTVAL('hibernate_sequence'), '02','dragon', '男', '汉', '1976-02-06', '广州市公安局海珠分局', '2006-05-18', '2026-05-18', 'address',now(),9);
+-- 查询
+SELECT b.*,a.* FROM bc_idcard a left join bc_idcard_check b on b.pid=a.id;
+select * from bc_idcard_pic;
+select * from bs_temp_driver order by file_date desc;
