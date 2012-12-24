703ec7a8618488100e12320d488f8d02c62842b7
dragon <rongjihuang@gmail.com>
2012-12-24 10:56:42 +0800

    邮件发送增加选项配置参数，避免xml配置需要重启服务器

--- bc-message/src/main/java/cn/bc/mail/MailServiceImpl.java
+++ bc-message/src/main/java/cn/bc/mail/MailServiceImpl.java
@@ -1,12 +1,21 @@
 package cn.bc.mail;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import cn.bc.option.service.OptionService;
 
 /**
  * 邮件Service接口的默认实现
@@ -17,9 +26,11 @@ import org.springframework.mail.SimpleMailMessage;
 public class MailServiceImpl implements MailService {
 	private static Log logger = LogFactory.getLog(MailServiceImpl.class);
 	private boolean async = false;// 是否异步发送邮件：默认同步，设为true异步发送
+	private OptionService optionService;
 	private MailSender mailSender;
 	private SimpleMailMessage templateMessage;
 	private TaskExecutor taskExecutor;// Spring异步执行器
+	private Map<String, String> mailConfig;// 邮件配置
 
 	public void setTaskExecutor(TaskExecutor taskExecutor) {
 		this.taskExecutor = taskExecutor;
@@ -31,6 +42,10 @@ public class MailServiceImpl implements MailService {
 
 	public void setTemplateMessage(SimpleMailMessage templateMessage) {
 		this.templateMessage = templateMessage;
+	}
+
+	public void setOptionService(OptionService optionService) {
+		this.optionService = optionService;
 	}
 
 	public void setAsync(boolean async) {
@@ -45,7 +60,17 @@ public class MailServiceImpl implements MailService {
 			throw new MailSendException("mail.to couldn't be null");
 		}
 
-		if (async) { // 异步发送
+		// 获取邮件发送配置
+		mailConfig = this.optionService
+				.findActiveOptionItemByGroupKey("bc.mailSender");
+
+		if (mailConfig != null) {
+			if (mailConfig.containsKey("async")) {
+				this.setAsync(Boolean.parseBoolean(mailConfig.get("async")));// 异步发送配置
+			}
+		}
+
+		if (this.async) { // 异步发送
 			this.sendByAsynchronousMode(mail);
 		} else { // 同步发送
 			this.sendBySynchronousMode(mail);
@@ -56,6 +81,14 @@ public class MailServiceImpl implements MailService {
 	private void sendBySynchronousMode(Mail mail) throws MailException {
 		// 复制一个线程安全的邮件消息
 		SimpleMailMessage msg = new SimpleMailMessage(this.templateMessage);
+		if (mailConfig != null) {
+			if (mailConfig.containsKey("username")) {
+				msg.setFrom(mailConfig.get("username"));// 邮件发送人
+			}
+			if (mailConfig.containsKey("subject")) {// 默认邮件标题
+				msg.setSubject(mailConfig.get("subject"));
+			}
+		}
 
 		msg.setSubject(mail.getSubject()); // 标题
 		msg.setText(mail.getContent()); // 内容
@@ -64,8 +97,45 @@ public class MailServiceImpl implements MailService {
 		msg.setCc(mail.getCc()); // 抄送
 		msg.setBcc(mail.getBcc()); // 密送
 
+		// 根据选项配置初始化邮件发送器
+		this.initMailSender();
+
 		// 发送邮件
 		this.mailSender.send(msg);
+	}
+
+	private void initMailSender() {
+		if (!(mailSender instanceof JavaMailSenderImpl) || mailConfig == null)
+			return;
+
+		// 按配置设置
+		JavaMailSenderImpl javaMailSender = (JavaMailSenderImpl) mailSender;
+		if (mailConfig.containsKey("host"))
+			javaMailSender.setHost(mailConfig.get("host"));
+		if (mailConfig.containsKey("port"))
+			javaMailSender.setPort(Integer.parseInt(mailConfig.get("port")));
+		if (mailConfig.containsKey("defaultEncoding"))
+			javaMailSender
+					.setDefaultEncoding(mailConfig.get("defaultEncoding"));
+		if (mailConfig.containsKey("username"))
+			javaMailSender.setUsername(mailConfig.get("username"));
+		if (mailConfig.containsKey("password"))
+			javaMailSender.setPassword(mailConfig.get("password"));
+		if (mailConfig.containsKey("javaMailProperties")) {
+			try {
+				JSONObject json = new JSONObject(mailConfig.get("javaMailProperties"));
+				Properties ps = javaMailSender.getJavaMailProperties();
+				@SuppressWarnings("unchecked")
+				Iterator<String> itor = json.keys();
+				String key;
+				while (itor.hasNext()) {
+					key = itor.next();
+					ps.setProperty(key, json.getString(key));
+				}
+			} catch (JSONException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
 	}
 
 	// 异步发送邮件
