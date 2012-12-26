0d547398665f1fe121878645d2199f28e2d81290
dragon <rongjihuang@gmail.com>
2012-12-26 17:32:12 +0800

    SpringUtils增加invokeBeanMethod方法

--- bc-core/src/main/java/cn/bc/core/util/SpringUtils.java
+++ bc-core/src/main/java/cn/bc/core/util/SpringUtils.java
@@ -15,11 +15,16 @@
  */
 package cn.bc.core.util;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.MethodInvoker;
+
+import cn.bc.core.exception.CoreException;
 
 /**
  * Spring的辅助函数库
@@ -44,6 +49,17 @@ public class SpringUtils implements ApplicationContextAware {
 	 * 
 	 * @param name
 	 *            bean的配置名称
+	 * @return bean对象
+	 */
+	public static Object getBean(String name) {
+		return applicationContext.getBean(name);
+	}
+
+	/**
+	 * 获取Bean对象
+	 * 
+	 * @param name
+	 *            bean的配置名称
 	 * @param requiredType
 	 *            bean的类型
 	 * @return bean对象
@@ -61,5 +77,49 @@ public class SpringUtils implements ApplicationContextAware {
 	 */
 	public static <T> T getBean(Class<T> requiredType) {
 		return applicationContext.getBean(requiredType);
+	}
+
+	/**
+	 * 运行Bean中指定名称的方法并返回其返回值
+	 * 
+	 * @param object
+	 *            object对象
+	 * @param methodName
+	 *            方法名
+	 * @param arguments
+	 *            参数值
+	 * @return 运行bean中指定名称的方法的返回值
+	 * @throws IllegalAccessException
+	 * @throws InvocationTargetException
+	 * @throws NoSuchMethodException
+	 * @throws ClassNotFoundException
+	 */
+	public static Object invokeBeanMethod(String beanName, String methodName,
+			Object[] arguments) {
+		try {
+			// 初始化
+			MethodInvoker methodInvoker = new MethodInvoker();
+			methodInvoker.setTargetObject(SpringUtils.getBean(beanName));
+			methodInvoker.setTargetMethod(methodName);
+
+			// 设置参数
+			if (arguments != null && arguments.length > 0) {
+				methodInvoker.setArguments(arguments);
+			}
+
+			// 准备方法
+			methodInvoker.prepare();
+
+			// 执行方法
+			return methodInvoker.invoke();
+		} catch (ClassNotFoundException e) {
+			throw new CoreException(e);
+		} catch (NoSuchMethodException e) {
+			throw new CoreException(e);
+		} catch (InvocationTargetException e) {
+			throw new CoreException(e);
+		} catch (IllegalAccessException e) {
+			throw new CoreException(e);
+		}
 	}
 }
