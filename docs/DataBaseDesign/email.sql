9fec57ee870312616ef2bf1014dfca0e88a02c6c
fatfish <fatfish2010@sina.cn>
2013-03-28 17:13:58 +0800

    邮件sql脚本修改

--- docs/DataBaseDesign/email.sql
+++ docs/DataBaseDesign/email.sql
@@ -37,6 +37,7 @@ CREATE TABLE BC_EMAIL_TO(
 	PID INT DEFAULT 0 NOT NULL,
 	TYPE_ INT NOT NULL,
 	RECEIVER_ID INT NOT NULL,
+	UPPER_ID INT,
 	ORDER_ INT DEFAULT 0 NOT NULL,
 	READ_ BOOLEAN DEFAULT 'false' NOT NULL,
 	CONSTRAINT BCPK_EMAIL_TO PRIMARY KEY (ID)
@@ -46,11 +47,14 @@ COMMENT ON COLUMN BC_EMAIL_TO.ID IS 'ID';
 COMMENT ON COLUMN BC_EMAIL_TO.PID IS '邮件ID';
 COMMENT ON COLUMN BC_EMAIL_TO.TYPE_ IS '发送类型 : 0-主送,1-抄送,2-密送';
 COMMENT ON COLUMN BC_EMAIL_TO.RECEIVER_ID IS '收件人ID : 对应Actor的ID';
+COMMENT ON COLUMN BC_EMAIL_TO.UPPER_ID IS '收件人所属岗位或单位或部门的ID : 对应Actor的ID';
 COMMENT ON COLUMN BC_EMAIL_TO.ORDER_ IS '排序号 : 针对同一发送类型的不同收件人之间的排序';
 COMMENT ON COLUMN BC_EMAIL_TO.READ_ IS '已阅标记';
 ALTER TABLE BC_EMAIL_TO ADD CONSTRAINT BCFK_EMAIL_TO_PID FOREIGN KEY (PID)
 	REFERENCES BC_EMAIL (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE BC_EMAIL_TO ADD CONSTRAINT BCFK_EMAIL_TO_RECEIVER FOREIGN KEY (RECEIVER_ID)
+	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE BC_EMAIL_TO ADD CONSTRAINT BCFK_EMAIL_TO_UPPER FOREIGN KEY (UPPER_ID)
 	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 CREATE TABLE BC_EMAIL_HISTORY(
@@ -63,16 +67,17 @@ CREATE TABLE BC_EMAIL_HISTORY(
 COMMENT ON TABLE BC_EMAIL_HISTORY IS '邮件查阅历史';
 COMMENT ON COLUMN BC_EMAIL_HISTORY.ID IS 'ID';
 COMMENT ON COLUMN BC_EMAIL_HISTORY.PID IS '邮件ID';
-COMMENT ON COLUMN BC_EMAIL_HISTORY.READER_ID IS '查阅人ID : 对应Actor的ID';
+COMMENT ON COLUMN BC_EMAIL_HISTORY.READER_ID IS '查阅人ID : 对应ActorHistory的ID';
 COMMENT ON COLUMN BC_EMAIL_HISTORY.FILE_DATE IS '查阅时间';
 ALTER TABLE BC_EMAIL_HISTORY ADD CONSTRAINT BCFK_EMAIL_HISTORY_PID FOREIGN KEY (PID)
 	REFERENCES BC_EMAIL (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE BC_EMAIL_HISTORY ADD CONSTRAINT BCFK_EMAIL_HISTORY_READER FOREIGN KEY (READER_ID)
-	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+	REFERENCES BC_IDENTITY_ACTOR_HISTORY (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 CREATE TABLE BC_EMAIL_TRASH(
 	ID INT NOT NULL,
 	PID INT NOT NULL,
+	SRC INT NOT NULL,
 	OWNER_ID INT NOT NULL,
 	STATUS_ INT DEFAULT 0 NOT NULL,
 	HANDLE_DATE TIMESTAMP NOT NULL,
@@ -81,6 +86,7 @@ CREATE TABLE BC_EMAIL_TRASH(
 COMMENT ON TABLE BC_EMAIL_TRASH IS '邮件垃圾箱';
 COMMENT ON COLUMN BC_EMAIL_TRASH.ID IS 'ID';
 COMMENT ON COLUMN BC_EMAIL_TRASH.PID IS '邮件ID';
+COMMENT ON COLUMN BC_EMAIL_TRASH.SRC IS '来源:1-发件箱 2-收件箱';
 COMMENT ON COLUMN BC_EMAIL_TRASH.OWNER_ID IS '所有者ID : 对应Actor的ID';
 COMMENT ON COLUMN BC_EMAIL_TRASH.STATUS_ IS '状态 : 0-可恢复,1-已删除';
 COMMENT ON COLUMN BC_EMAIL_TRASH.HANDLE_DATE IS '操作时间';
@@ -88,3 +94,102 @@ ALTER TABLE BC_EMAIL_TRASH ADD CONSTRAINT BCFK_EMAIL_TRASH_PID FOREIGN KEY (PID)
 	REFERENCES BC_EMAIL (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE BC_EMAIL_TRASH ADD CONSTRAINT BCFK_EMAIL_TRASH_OWNER FOREIGN KEY (OWNER_ID)
 	REFERENCES BC_IDENTITY_ACTOR (ID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- 资源配置：我的邮件
+insert into BC_IDENTITY_RESOURCE (ID,STATUS_,INNER_,TYPE_,BELONG,ORDER_,NAME,URL,ICONCLASS) 
+    select NEXTVAL('CORE_SEQUENCE'), 0, false, 1, m.id, '010400','我的邮件', null, 'i0004' from BC_IDENTITY_RESOURCE m where m.order_='010000'
+		and not exists(select 1 from bc_identity_resource where order_='010400');
+
+-- 资源配置：收件箱
+insert into BC_IDENTITY_RESOURCE (ID,STATUS_,INNER_,TYPE_,BELONG,ORDER_,NAME,URL,ICONCLASS) 
+	select NEXTVAL('CORE_SEQUENCE'), 0, false, 2, m.id, '010410','收件箱', '/bc/emailTos/paging', 'i0408' from BC_IDENTITY_RESOURCE m where m.order_='010400'
+	and not exists(select 1 from bc_identity_resource where order_='010410');
+
+-- 资源配置：发件箱
+insert into BC_IDENTITY_RESOURCE (ID,STATUS_,INNER_,TYPE_,BELONG,ORDER_,NAME,URL,ICONCLASS) 
+	select NEXTVAL('CORE_SEQUENCE'), 0, false, 2, m.id, '010420','发件箱', '/bc/emailSends/paging', 'i0409' from BC_IDENTITY_RESOURCE m where m.order_='010400'
+	and not exists(select 1 from bc_identity_resource where order_='010420');
+
+-- 资源配置：垃圾箱
+insert into BC_IDENTITY_RESOURCE (ID,STATUS_,INNER_,TYPE_,BELONG,ORDER_,NAME,URL,ICONCLASS) 
+	select NEXTVAL('CORE_SEQUENCE'), 0, false, 2, m.id, '010430','垃圾箱', '/bc/emailTrashs/paging', 'i0409' from BC_IDENTITY_RESOURCE m where m.order_='010400'
+	and not exists(select 1 from bc_identity_resource where order_='010430');
+
+-- 权限访问配置：通用角色
+insert into BC_IDENTITY_ROLE_RESOURCE (RID,SID) 
+	select r.id,m.id from BC_IDENTITY_ROLE r,BC_IDENTITY_RESOURCE m where r.code='BC_COMMON' 
+	and m.type_ > 1 and m.order_ in ('010410','010420','010430')
+	and not exists(select 1 from BC_IDENTITY_ROLE_RESOURCE 
+				where rid=(select r2.id from BC_IDENTITY_ROLE r2 where r2.code='BC_COMMON')
+				and sid in(select m2.id from BC_IDENTITY_RESOURCE m2 where m2.type_ > 1 and m2.order_ in ('010410','010420','010430')))
+	order by m.order_;
+
+-- 获取邮件的收件人
+CREATE OR REPLACE FUNCTION getemailreceiver2json(emailid INTEGER)
+	RETURNS VARCHAR  AS
+$BODY$
+DECLARE
+	-- 定义变量
+	-- 收件人
+	receiver CHARACTER VARYING;
+	receiver_upper_id INTEGER;
+	-- 抄送
+	cc CHARACTER VARYING;
+	cc_upper_id INTEGER;
+	-- 密送
+	bcc CHARACTER VARYING;
+	bcc_upper_id INTEGER;
+	-- 变量一行结果的记录	
+	rowinfo RECORD;
+BEGIN
+	receiver := '';
+	cc := '';
+	bcc := '';
+
+	-- 循环集合
+	FOR rowinfo IN SELECT t.upper_id,a.name aname,u.name uname,t.type_ 
+			FROM bc_email_to t
+			INNER JOIN bc_identity_actor a on a.id=t.receiver_id
+			LEFT JOIN bc_identity_actor u on u.id=t.upper_id
+			WHERE t.pid=emailid
+			ORDER BY t.type_,t.order_
+	LOOP
+		-- 类型为收件人时
+		IF rowinfo.type_=0 THEN
+			-- 先判断是否带上级组织 
+			IF rowinfo.upper_id IS NULL THEN
+				-- 判断是否添加逗号
+				IF char_length(receiver)>0 THEN receiver := receiver||','; END IF;
+				-- 拼接收件人名称字符串
+				receiver := receiver||rowinfo.aname;
+			-- 相同上级组织的收件人只显示上级组织名称
+			ELSEIF rowinfo.upper_id != receiver_upper_id OR receiver_upper_id IS NULL THEN
+				IF char_length(receiver)>0 THEN receiver := receiver||','; END IF;
+				receiver := receiver||rowinfo.uname;
+				receiver_upper_id := rowinfo.upper_id;
+			END IF;		
+		ELSEIF rowinfo.type_=1 THEN
+			IF rowinfo.upper_id IS NULL THEN
+				IF char_length(cc)>0 THEN cc := cc||','; END IF;
+				cc := cc||rowinfo.aname;
+			ELSEIF rowinfo.upper_id != cc_upper_id OR cc_upper_id IS NULL THEN
+				IF char_length(cc)>0 THEN cc := cc||','; END IF;
+				cc := cc||rowinfo.uname;
+				cc_upper_id := rowinfo.upper_id;
+			END IF;
+		ELSEIF rowinfo.type_=2 THEN
+			IF rowinfo.upper_id IS NULL THEN
+				IF char_length(bcc)>0 THEN bcc := bcc||','; END IF;
+				bcc := bcc||rowinfo.aname;
+			ELSEIF rowinfo.upper_id != bcc_upper_id OR bcc_upper_id IS NULL THEN
+				IF char_length(bcc)>0 THEN bcc := bcc||','; END IF;
+				bcc := bcc||rowinfo.uname;
+				bcc_upper_id := rowinfo.upper_id;
+			END IF;
+		END IF;
+	END LOOP;
+	
+	RETURN '{"receiver":"'||receiver||'","cc":"'||cc||'","bcc":"'||bcc||'"}';
+END;
+$BODY$
+ LANGUAGE plpgsql;
