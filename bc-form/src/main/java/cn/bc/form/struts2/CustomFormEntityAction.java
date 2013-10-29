c02990997ff803dce5e33d973e740d551bdca0a7
tmax37 <298061836@qq.com>
2013-10-29 16:55:50 +0800

    新增额外参数接口，支持自定义表单保存，增加表单只读状态判断

--- bc-form/src/main/java/cn/bc/form/struts2/CustomFormEntityAction.java
+++ bc-form/src/main/java/cn/bc/form/struts2/CustomFormEntityAction.java
@@ -3,6 +3,7 @@ package cn.bc.form.struts2;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +13,6 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.RequestAware;
 import org.apache.struts2.interceptor.SessionAware;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -21,6 +21,10 @@ import org.springframework.stereotype.Controller;
 
 import cn.bc.BCConstants;
 import cn.bc.Context;
+import cn.bc.core.exception.ConstraintViolationException;
+import cn.bc.core.exception.InnerLimitedException;
+import cn.bc.core.exception.NotExistsException;
+import cn.bc.core.exception.PermissionDeniedException;
 import cn.bc.core.util.DateUtils;
 import cn.bc.core.util.SpringUtils;
 import cn.bc.core.util.StringUtils;
@@ -38,7 +42,6 @@ import cn.bc.identity.web.SystemContext;
 import cn.bc.identity.web.SystemContextHolder;
 import cn.bc.template.engine.TemplateEngine;
 import cn.bc.template.service.TemplateService;
-import cn.bc.template.util.FreeMarkerUtils;
 import cn.bc.web.ui.json.Json;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -67,23 +70,30 @@ public class CustomFormEntityAction extends ActionSupport implements
 	private AttachService attachService;
 	public AttachWidget attachsUI;
 	public String json;
-
+	public String type; // 表单类型
+	public String code; // 编码
+	public long pid;
 	/**
 	 * 模板编码 如果含字符":"，则进行分拆，前面部分为编码， 后面部分为版本号，如果没有字符":"，将获取当前状态为正常的版本后格式化
 	 */
 	public String tpl;
 
 	/**
-	 * 自定义表单数据，使用标准的Json数据格式：[{name:"",value:"",type:"int|long|string|date|..."}
-	 * ]
+	 * 自定义表单数据，使用标准的Json数据格式：{name:"",value:"",type:"int|long|string|date|..."}
+	 * 
 	 */
 	public String formData;
 
 	/**
-	 * 自定义表单信息，使用标准的Json数据格式：[{id:"",uid:"",type:"",status:"",subject:"",tpl:"",
-	 * authorId:"",fileDate:"",modifierId:"",modifiedDate:""}]
+	 * 自定义表单信息，使用标准的Json数据格式：{id:"",uid:"",type:"",status:"",subject:"",tpl:"",
+	 * authorId:"",fileDate:"",modifierId:"",modifiedDate:""}
 	 */
 	public String formInfo;
+	
+	/** 
+	 * 创建自定义表单时的额外参数,使用标准Json数据格式[{var : value,type : valueType}]
+	 */
+	public String extraData;
 
 	public void setRequest(Map<String, Object> request) {
 		this.request = request;
@@ -150,41 +160,14 @@ public class CustomFormEntityAction extends ActionSupport implements
 	}
 
 	// 渲染表单
-	public String render() {
-		JSONObject formInfoJO;
-		String type = "";
-		Long pid = 0L;
-		String code = "";
-		try {
-			formInfoJO = new JSONObject(this.formInfo);
-			type = formInfoJO.getString("type");
-			pid = formInfoJO.getLong("pid");
-			code = formInfoJO.getString("code");
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-
-		Form f = this.formService.findForm(type, pid, code);
-		JSONObject jo = new JSONObject();
+	public String render() throws Exception {
+		Form f = this.formService.findByTPC(type, pid, code);
 		if (f == null) {
-			try {
-				jo.put("success", false);
-				jo.put("msg", "不存在此记录");
-				this.json = jo.toString();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
+			return this.create();
 		} else {
-			try {
-				jo.put("success", true);
-				jo.put("msg", "存在此记录");
-				jo.put("id", f.getId());
-				this.json = jo.toString();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
+			this.id = f.getId();
+			return this.edit();
 		}
-		return "json";
 	}
 
 	// 创建自定义表单
@@ -213,6 +196,28 @@ public class CustomFormEntityAction extends ActionSupport implements
 		infoArgs.put("authorId", author.getId());
 		infoArgs.put("fileDate", fileDate);
 		args.put("form_info", infoArgs.toString());
+		
+		//设置额外参数
+		if(this.extraData != null && !this.extraData.equals("")) {
+			JSONArray extraDataJA = new JSONArray(this.extraData);
+			for(int i=0; i<extraDataJA.length(); i++) {
+				JSONObject extraDataJO = extraDataJA.getJSONObject(i);
+				Iterator it = extraDataJO.keys();
+				String type = "";
+				String name = "";
+				while (it.hasNext()) {  
+					String key = (String) it.next();
+					if(key.equals("type")) {
+						type = extraDataJO.getString("type");
+					} else {
+						name = key;
+					}
+				}
+				args.put(name,StringUtils.convertValueByType(type,
+						extraDataJO.getString(name)));
+			}
+		}
+		
 
 		addSystemContextParam(args);
 		formatHtml(this.tpl, args);
@@ -298,10 +303,6 @@ public class CustomFormEntityAction extends ActionSupport implements
 
 	// 编辑自定义表单
 	public String edit() throws Exception {
-		if (this.id == null) {
-			throw new CoreException("Must set property id!");
-		}
-
 		// 根据自定义表单id，获取相应的自定义表单表单对象，根据表单字段参数格式化模板后生成的前台表单HTML代码
 		Form form = this.formService.load(this.id);
 		// 获取模板文本
@@ -353,7 +354,10 @@ public class CustomFormEntityAction extends ActionSupport implements
 		List<Field> fields = this.fieldService.findList(form);
 		if (fields != null && fields.size() != 0) {
 			for (Field f : fields) {
-				args.put(f.getName(),StringUtils.convertValueByType(f.getType(),f.getValue()));
+				args.put(
+						f.getName(),
+						StringUtils.convertValueByType(f.getType(),
+								f.getValue()));
 			}
 		}
 
@@ -362,17 +366,157 @@ public class CustomFormEntityAction extends ActionSupport implements
 		return "page";
 	}
 
+	// 删除自定义表单
+	public String delete() throws Exception {
+		Json _json = new Json();
+		try {
+			if (this.id != null) {// 删除一条
+				// 根据自定义表单id，获取相应的自定义表单表单对象，根据表单字段参数格式化模板后生成的前台表单HTML代码
+				Form form = this.formService.load(this.id);
+
+				// 获取表单字段属性
+				List<Field> fields = this.fieldService.findList(form);
+				if (fields != null && fields.size() != 0) {
+					for (Field f : fields) {
+						this.fieldService.delete(f.getId());
+					}
+
+				}
+				this.formService.delete(form.getId());
+			} else if (this.ids != null && this.ids.length() > 0) {// 删除一批
+
+				Long[] ids = cn.bc.core.util.StringUtils
+						.stringArray2LongArray(this.ids.split(","));
+				for (int i = 0; i < ids.length; i++) {
+					// 根据自定义表单id，获取相应的自定义表单表单对象，根据表单字段参数格式化模板后生成的前台表单HTML代码
+					Form form = this.formService.load(ids[i]);
+
+					// 获取表单字段属性
+					List<Field> fields = this.fieldService.findList(form);
+					if (fields != null && fields.size() != 0) {
+						for (Field f : fields) {
+							this.fieldService.delete(f.getId());
+						}
+
+					}
+					this.formService.delete(form.getId());
+				}
+			} else if(this.type != null && !this.type.equals("") && this.pid != 0L && this.code != null && !this.code.equals("") ){
+				// 根据自定义表单id，获取相应的自定义表单表单对象，根据表单字段参数格式化模板后生成的前台表单HTML代码
+				Form form = this.formService.findByTPC(type, pid, code);
+
+				// 获取表单字段属性
+				List<Field> fields = this.fieldService.findList(form);
+				if (fields != null && fields.size() != 0) {
+					for (Field f : fields) {
+						this.fieldService.delete(f.getId());
+					}
+
+				}
+				this.formService.delete(form.getId());
+			} else {
+				throw new CoreException("must set property id or ids");
+			}
+
+			_json.put("success", true);
+			_json.put("msg", getText("form.delete.success"));
+			json = _json.toString();
+			return "json";
+		} catch (PermissionDeniedException e) {
+			// 执行没有权限的操作
+			_json.put("msg", getDeleteExceptionMsg(e));
+			_json.put("e", e.getClass().getSimpleName());
+		} catch (InnerLimitedException e) {
+			// 删除内置对象
+			_json.put("msg", getDeleteExceptionMsg(e));
+			_json.put("e", e.getClass().getSimpleName());
+		} catch (NotExistsException e) {
+			// 执行没有权限的操作
+			_json.put("msg", getDeleteExceptionMsg(e));
+			_json.put("e", e.getClass().getSimpleName());
+		} catch (ConstraintViolationException e) {
+			// 违反约束关联引发的异常
+			_json.put("msg", getDeleteExceptionMsg(e));
+			_json.put("e", e.getClass().getSimpleName());
+		} catch (Exception e) {
+			// 其他异常
+			dealOtherDeleteException(_json, e);
+		}
+		_json.put("success", false);
+		json = _json.toString();
+
+		return "json";
+	}
+
 	// 查看自定表单
 	public String open() throws Exception {
+		// 根据自定义表单id，获取相应的自定义表单表单对象，根据表单字段参数格式化模板后生成的前台表单HTML代码
+		Form form = this.formService.findByTPC(type, pid, code);
+		// 获取模板文本
+		String content = this.templateService.getContent(form.getTpl());
 
-		// 构建附件控件
-		attachsUI = buildAttachsUI(false, true);
+		// 构建格式化模板参数
+		Map<String, Object> args = new HashMap<String, Object>();
+		ActorHistory author = form.getAuthor();
+		ActorHistory modifier = form.getModifier();
+		String fileDate = DateUtils.formatCalendar2Second(form.getFileDate());
+		String modifiedDate = DateUtils.formatCalendar2Second(form
+				.getModifiedDate());
+		String uid = form.getUid();
+		String type = form.getType();
+		String code = form.getCode();
+		Long pid = form.getPid();
+		String subject = form.getSubject();
+
+		args.put("form_author", author.getName());
+		args.put("form_fileDate", fileDate);
+		args.put("form_modifier", modifier.getName());
+		args.put("form_modifiedDate", modifiedDate);
+		args.put("form_uid", uid);
+		args.put("form_status", form.getStatus());
+		args.put("form_isNew", false);
+		args.put("form_type", type);
+		args.put("form_code", code);
+		args.put("form_pid", pid);
+		args.put("form_subject", subject);
+		args.put("form_id", id);
+
+		// 设置${from_info}参数对应的值
+		JSONObject infoJson = new JSONObject();
+		infoJson.put("author", author.getName());
+		infoJson.put("fileDate", fileDate);
+		infoJson.put("modifier", modifier.getName());
+		infoJson.put("modifiedDate", modifiedDate);
+		infoJson.put("uid", uid);
+		infoJson.put("status", form.getStatus());
+		infoJson.put("isNew", false);
+		infoJson.put("type", type);
+		infoJson.put("code", code);
+		infoJson.put("pid", pid);
+		infoJson.put("subject", subject);
+		infoJson.put("id", id);
+		args.put("form_info", infoJson.toString());
+
+		// 获取表单字段属性
+		List<Field> fields = this.fieldService.findList(form);
+		if (fields != null && fields.size() != 0) {
+			for (Field f : fields) {
+				args.put(
+						f.getName(),
+						StringUtils.convertValueByType(f.getType(),
+								f.getValue()));
+			}
+		}
+
+		addSystemContextParam(args);
+		formatHtml(this.tpl, args);
 		return "page";
 	}
 
 	protected AttachWidget buildAttachsUI(boolean isNew, boolean forceReadonly) {
+
 		/*
-		 * // 构建附件控件 String ptype = "bulletin.main"; String puid =
+		 * * // 构建附件控件 String ptype = "bulletin.main"; String puid =
 		 * this.getE().getUid(); boolean readonly = forceReadonly ? true :
 		 * this.isReadonly(); AttachWidget attachsUI =
 		 * AttachWidget.defaultAttachWidget(isNew, readonly, isFlashUpload(),
@@ -382,6 +526,7 @@ public class CustomFormEntityAction extends ActionSupport implements
 		 * .setMaxCount(Integer.parseInt(getText("app.attachs.maxCount")))
 		 * .setMaxSize(Integer.parseInt(getText("app.attachs.maxSize")));
 		 */
+
 		return attachsUI;
 	}
 
@@ -399,5 +544,43 @@ public class CustomFormEntityAction extends ActionSupport implements
 	public static boolean isIE() {
 		return ServletActionContext.getRequest().getHeader("User-Agent")
 				.toUpperCase().indexOf("MSIE") != -1;
+	}
+
+	/**
+	 * 获取删除操作的异常提示信息
+	 * 
+	 * @return
+	 */
+	protected String getDeleteExceptionMsg(Exception e) {
+		if (e instanceof PermissionDeniedException) {
+			return getText("exception.delete.permissionDenied");
+		} else if (e instanceof InnerLimitedException) {
+			return getText("exception.delete.innerLimited");
+		} else if (e instanceof NotExistsException) {
+			return getText("exception.delete.notExists");
+		} else if (e instanceof ConstraintViolationException) {
+			return getText("exception.delete.constraintViolation");
+		} else {
+			return e.getMessage();
+		}
+	}
+
+	/**
+	 * 删除操作平台没有处理的异常的默认处理
+	 * 
+	 * @param json
+	 * @param e
+	 */
+	protected void dealOtherDeleteException(Json json, Exception e) {
+		if ((e.getCause() != null && e.getCause() instanceof org.hibernate.exception.ConstraintViolationException)
+				|| (e.getCause().getCause() != null && e.getCause().getCause() instanceof org.hibernate.exception.ConstraintViolationException)) {
+			// 违反约束关联引发的异常
+			json.put("msg", getText("exception.delete.constraintViolation"));
+			json.put("e", e.getClass().getSimpleName());
+		} else {
+			// 其他异常
+			json.put("msg", e.toString());
+			json.put("e", e.getClass().getSimpleName());
+		}
 	}
 }
