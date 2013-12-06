3ca3ba72a9ee02d07dc9e61a96d161d9959e055b
luliang <a4306150@163.com>
2013-12-06 16:08:55 +0800

    提交DeviceEventAction修改

--- bc-device/src/main/java/cn/bc/device/web/struts2/DeviceEventAction.java
+++ bc-device/src/main/java/cn/bc/device/web/struts2/DeviceEventAction.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-
-import cn.bc.device.domain.DeviceEventDomain;
+import cn.bc.device.domain.DeviceEventEntity;
 import cn.bc.device.service.DeviceEventService;
 import cn.bc.web.struts2.EntityAction;
 
@@ -17,7 +16,7 @@ import cn.bc.web.struts2.EntityAction;
  */
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Controller
-public class DeviceEventAction extends EntityAction<Long, DeviceEventDomain> {
+public class DeviceEventAction extends EntityAction<Long, DeviceEventEntity> {
 
 	private static final long serialVersionUID = 1L;
 	private DeviceEventService deviceEventService;
