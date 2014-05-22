d941b6cd8075421dac85386b945e025154ad01f0
dragon <rongjihuang@gmail.com>
2014-05-22 11:52:31 +0800

    视图导出Excel增加可扩展自定义参数的功能

--- bc-web/src/main/java/cn/bc/web/ui/html/grid/GridExporter.java
+++ bc-web/src/main/java/cn/bc/web/ui/html/grid/GridExporter.java
@@ -24,167 +24,203 @@ import cn.bc.core.util.DateUtils;
 
 /**
  * Grid数据的Excel导出实现
- * 
+ *
  * @author dragon
- * 
  */
 public class GridExporter implements Exporter {
-	private static final Log logger = LogFactory.getLog(GridExporter.class);
-	private InputStream templateFile; // 模板文件
-	private String title; // 标题
-	private String idLabel; // id列的列标题
-	private List<Column> columns;
-	private List<? extends Object> data;
-	private ExpressionParser parser;
+    private static final Log logger = LogFactory.getLog(GridExporter.class);
+    private InputStream templateFile; // 模板文件
+    private String title; // 标题
+    private String idLabel; // id列的列标题
+    private List<Column> columns;
+    private List<? extends Object> data;
+    private ExpressionParser parser;
+    private Map<String, Object> extras;// 额外的数据
 
-	public GridExporter() {
+    public GridExporter() {
 
-	}
+    }
 
-	public String getIdLabel() {
-		return idLabel;
-	}
+    public String getIdLabel() {
+        return idLabel;
+    }
 
-	public GridExporter setIdLabel(String idLabel) {
-		this.idLabel = idLabel;
-		return this;
-	}
+    public GridExporter setIdLabel(String idLabel) {
+        this.idLabel = idLabel;
+        return this;
+    }
 
-	public ExpressionParser getParser() {
-		return parser;
-	}
+    public ExpressionParser getParser() {
+        return parser;
+    }
 
-	public GridExporter setParser(ExpressionParser parser) {
-		this.parser = parser;
-		return this;
-	}
+    public GridExporter setParser(ExpressionParser parser) {
+        this.parser = parser;
+        return this;
+    }
 
-	public List<Column> getColumns() {
-		return columns;
-	}
+    public List<Column> getColumns() {
+        return columns;
+    }
 
-	public GridExporter setColumns(List<Column> columns) {
-		this.columns = columns;
-		return this;
-	}
+    public GridExporter setColumns(List<Column> columns) {
+        this.columns = columns;
+        return this;
+    }
 
-	public List<? extends Object> getData() {
-		return data;
-	}
+    public List<? extends Object> getData() {
+        return data;
+    }
 
-	public GridExporter setData(List<? extends Object> data) {
-		this.data = data;
-		return this;
-	}
+    public GridExporter setData(List<? extends Object> data) {
+        this.data = data;
+        return this;
+    }
 
-	public InputStream getTemplateFile() {
-		return templateFile;
-	}
+    /**
+     * 获取以添加的额外数据
+     * @return
+     */
+    public Map<String, Object> getExtras() {
+        return extras;
+    }
 
-	public GridExporter setTemplateFile(InputStream templateFile) {
-		this.templateFile = templateFile;
-		return this;
-	}
+    /**
+     * 添加一个额外数据
+     * @param key 键
+     * @param value 值
+     */
+    public GridExporter addExtra(String key, Object value) {
+        if (this.extras == null) this.extras = new HashMap<String, Object>();
+        this.extras.put(key, value);
+        return this;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    /**
+     * 添加一份额外数据
+     * @param extras
+     */
+    public GridExporter addExtra(Map<String, Object> extras) {
+        if (this.extras == null) this.extras = new HashMap<String, Object>();
+        this.extras.putAll(extras);
+        return this;
+    }
 
-	public GridExporter setTitle(String title) {
-		this.title = title;
-		return this;
-	}
+    public InputStream getTemplateFile() {
+        return templateFile;
+    }
 
-	public void exportTo(OutputStream outputStream) throws Exception {
-		Date startTime = new Date();
-		if (logger.isDebugEnabled())
-			logger.debug("templateFile=" + this.getTemplateFile());
-		XLSTransformer transformer = new XLSTransformer();
-		InputStream is = this.getTemplateFile();
-		if (is == null)
-			is = new ClassPathResource("cn/bc/web/template/export.xls")
-					.getInputStream();// 使用默认的模板
+    public GridExporter setTemplateFile(InputStream templateFile) {
+        this.templateFile = templateFile;
+        return this;
+    }
 
-		if (logger.isDebugEnabled())
-			logger.debug("exportTo 1:" + DateUtils.getWasteTime(startTime));
-		transformer.transformXLS(is, this.parseData()).write(outputStream);
-		if (logger.isDebugEnabled())
-			logger.debug("exportTo 2:" + DateUtils.getWasteTime(startTime));
-		is.close();
-		outputStream.flush();
-		outputStream.close();
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void exportTo(String outputFile) throws Exception {
-		XLSTransformer transformer = new XLSTransformer();
-		Workbook workbook = transformer.transformXLS(this.getTemplateFile(),
-				this.parseData());
-		OutputStream os = new BufferedOutputStream(new FileOutputStream(
-				outputFile));
-		workbook.write(os);
-		this.getTemplateFile().close();
-		os.flush();
-		os.close();
-	}
+    public GridExporter setTitle(String title) {
+        this.title = title;
+        return this;
+    }
 
-	/**
-	 * 将Grid的数据转换为默认Excel模板需要的数据
-	 * 
-	 * @return
-	 */
-	protected Map<String, Object> parseData() {
-		Date startTime = new Date();
-		Map<String, Object> map = new HashMap<String, Object>();
+    public void exportTo(OutputStream outputStream) throws Exception {
+        Date startTime = new Date();
+        if (logger.isDebugEnabled())
+            logger.debug("templateFile=" + this.getTemplateFile());
+        XLSTransformer transformer = new XLSTransformer();
+        InputStream is = this.getTemplateFile();
+        if (is == null)
+            is = new ClassPathResource("cn/bc/web/template/export.xls")
+                    .getInputStream();// 使用默认的模板
 
-		// 导出时间
-		map.put("exportTime", DateUtils.formatDateTime(new Date()));
+        if (logger.isDebugEnabled())
+            logger.debug("exportTo 1:" + DateUtils.getWasteTime(startTime));
+        transformer.transformXLS(is, this.parseData()).write(outputStream);
+        if (logger.isDebugEnabled())
+            logger.debug("exportTo 2:" + DateUtils.getWasteTime(startTime));
+        is.close();
+        outputStream.flush();
+        outputStream.close();
+    }
 
-		// 标题
-		map.put("title", this.getTitle());
+    public void exportTo(String outputFile) throws Exception {
+        XLSTransformer transformer = new XLSTransformer();
+        Workbook workbook = transformer.transformXLS(this.getTemplateFile(),
+                this.parseData());
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(
+                outputFile));
+        workbook.write(os);
+        this.getTemplateFile().close();
+        os.flush();
+        os.close();
+    }
 
-		// 表头
-		Collection<String> columnNames = new ArrayList<String>();
-		for (Column column : columns) {
-			if (column instanceof IdColumn) {
-				columnNames.add(idLabel != null ? idLabel : "序号");
-			} else if (column instanceof HiddenColumn) {
-				columnNames.add("");// 空白
-			} else {
-				columnNames.add(column.getLabel());
-			}
-		}
-		map.put("columnNames", columnNames);
+    /**
+     * 将Grid的数据转换为默认Excel模板需要的数据
+     *
+     * @return
+     */
+    protected Map<String, Object> parseData() {
+        Date startTime = new Date();
+        Map<String, Object> map = new HashMap<String, Object>();
 
-		// 表格数据
-		Collection<Collection<Object>> rows = new ArrayList<Collection<Object>>();
-		Collection<Object> row;
-		Object value;
-		int i = 1;
-		for (Object rowData : data) {
-			row = new ArrayList<Object>();
-			for (Column column : columns) {
-				if (column instanceof IdColumn) {
-					value = i;
-					row.add(value);
-				} else {
-					value = GridData.formatValue2Label(
-							rowData,
-							GridData.getValue(rowData,
-									column.getValueExpression(), parser),
-							column.getValueFormater());
-					// row.add(value == null || value.length() == 0 ? "" :
-					// value);
-					// System.out.println(value.getClass());
-					row.add(value);
-				}
-			}
-			rows.add(row);
-			i++;
-		}
-		map.put("rowDatas", rows);
+        // 导出时间
+        Date now = new Date();
+        map.put("exportTime", DateUtils.formatDateTime(now));
+        map.put("exportTime_", now);
 
-		if (logger.isDebugEnabled())
-			logger.debug("parseData:" + DateUtils.getWasteTime(startTime));
-		return map;
-	}
+        // 标题
+        map.put("title", this.getTitle());
+
+        // 表头
+        Collection<String> columnNames = new ArrayList<String>();
+        for (Column column : columns) {
+            if (column instanceof IdColumn) {
+                columnNames.add(idLabel != null ? idLabel : "序号");
+            } else if (column instanceof HiddenColumn) {
+                columnNames.add("");// 空白
+            } else {
+                columnNames.add(column.getLabel());
+            }
+        }
+        map.put("columnNames", columnNames);
+
+        // 表格数据
+        Collection<Collection<Object>> rows = new ArrayList<Collection<Object>>();
+        Collection<Object> row;
+        Object value;
+        int i = 1;
+        for (Object rowData : data) {
+            row = new ArrayList<Object>();
+            for (Column column : columns) {
+                if (column instanceof IdColumn) {
+                    value = i;
+                    row.add(value);
+                } else {
+                    value = GridData.formatValue2Label(
+                            rowData,
+                            GridData.getValue(rowData,
+                                    column.getValueExpression(), parser),
+                            column.getValueFormater()
+                    );
+                    // row.add(value == null || value.length() == 0 ? "" :
+                    // value);
+                    // System.out.println(value.getClass());
+                    row.add(value);
+                }
+            }
+            rows.add(row);
+            i++;
+        }
+        map.put("rowDatas", rows);
+
+        if (this.extras != null) {
+            map.putAll(this.extras);
+        }
+
+        if (logger.isDebugEnabled())
+            logger.debug("parseData:" + DateUtils.getWasteTime(startTime));
+        return map;
+    }
 }
