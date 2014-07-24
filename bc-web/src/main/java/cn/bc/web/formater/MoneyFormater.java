a798dddfb701d08f5d27193b621aad4164812623
LeeDane <825711424@qq.com>
2014-07-24 15:11:09 +0800

    修改MoneyFormater类，根据需要是否显示¥符号

--- bc-web/src/main/java/cn/bc/web/formater/MoneyFormater.java
+++ bc-web/src/main/java/cn/bc/web/formater/MoneyFormater.java
@@ -13,9 +13,18 @@ import java.text.DecimalFormat;
  */
 public class MoneyFormater extends AbstractFormater<String> {
 	protected DecimalFormat format;
+	private String pattern = "##,###,###,###.#";
 
 	public MoneyFormater() {
-		format = new DecimalFormat("￥ ##,###,###,###.#");
+		format = new DecimalFormat("￥ " + pattern);
+	}
+
+	public MoneyFormater(boolean noSymble) {
+		if(noSymble){
+			format = new DecimalFormat(pattern);
+		}else{
+			format = new DecimalFormat("￥ " + pattern);
+		}
 	}
 
 	public MoneyFormater(String pattern) {
@@ -27,6 +36,16 @@ public class MoneyFormater extends AbstractFormater<String> {
 			return null;
 		if (value instanceof Number)
 			return format.format((Number) value);
+		else
+			return value.toString();
+	}
+
+	@Override
+	public String getExportText(Object context, Object value) {
+		if (value == null)
+			return null;
+		if (value instanceof Number)
+			return new DecimalFormat(pattern).format((Number) value);
 		else
 			return value.toString();
 	}
