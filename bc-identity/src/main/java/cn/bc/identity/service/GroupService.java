58eda094bb87a2db78d75ec310ad9235aed5da44
Action <572333932@qq.com>
2014-11-14 17:10:28 +0800

    添加通过岗位Names/Codes获得岗位集合

--- bc-identity/src/main/java/cn/bc/identity/service/GroupService.java
+++ bc-identity/src/main/java/cn/bc/identity/service/GroupService.java
@@ -2,6 +2,8 @@ package cn.bc.identity.service;
 
 import cn.bc.identity.domain.Actor;
 
+import java.util.List;
+
 /**
  * 岗位Service接口
  * 
@@ -12,8 +14,32 @@ public interface GroupService extends ActorService {
 	/**
 	 * 保存岗位信息
 	 * @param group 岗位
-	 * @param belongId 岗位隶属的上级Id
+	 * @param belongIds 岗位隶属的上级Id
 	 * @param userIds 岗位所分配的用户id
 	 */
 	Actor save(Actor group, Long[] belongIds, Long[] userIds);
+
+	/**
+	 * 通过岗位名称获得岗位集合
+	 *
+	 * @param belongIds 岗位隶属的上级Id
+	 * @param names 岗位名称
+	 * @param relationTypes  关联的类型，对应ActorRelation的type属性
+	 * @param followerTypes 岗位的类型，对应Actor的type属性
+	 * @return
+	 */
+	List<Actor> findByNames(Long[] belongIds, String[] names,
+			Integer[] relationTypes, Integer[] followerTypes);
+
+	/**
+	 * 通过岗位名称获得岗位集合
+	 *
+	 * @param belongIds 岗位隶属的上级Id
+	 * @param codes 岗位编码
+	 * @param relationTypes  关联的类型，对应ActorRelation的type属性
+	 * @param followerTypes 岗位的类型，对应Actor的type属性
+	 * @return
+	 */
+	List<Actor> findByCodes(Long[] belongIds, String[] codes,
+			Integer[] relationTypes, Integer[] followerTypes);
 }
