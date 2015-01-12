111bb091c89a4765713225b69c07131da0bc49d2
dragon <rongjihuang@gmail.com>
2015-01-12 13:46:21 +0800

    格式化模板的异常抛出而不是截获

--- bc-template/src/main/java/cn/bc/template/util/FreeMarkerUtils.java
+++ bc-template/src/main/java/cn/bc/template/util/FreeMarkerUtils.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import cn.bc.core.exception.CoreException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 使用FreeMarker的模板工具类
@@ -19,7 +21,7 @@ import org.apache.commons.logging.LogFactory;
  * 
  */
 public class FreeMarkerUtils {
-	protected static Log logger = LogFactory.getLog(FreeMarkerUtils.class);
+	protected static Logger logger = LoggerFactory.getLogger(FreeMarkerUtils.class);
 
 	private FreeMarkerUtils() {
 	}
