78e582287bbc085aa1122bf210dacfd3d6e658e2
dragon <rongjihuang@gmail.com>
2015-06-03 14:23:11 +0800

    修正编译错误

--- bc-web/src/main/java/cn/bc/web/ws/util/WSUtils.java
+++ bc-web/src/main/java/cn/bc/web/ws/util/WSUtils.java
@@ -1,29 +1,23 @@
 /**
- * 
+ *
  */
 package cn.bc.web.ws.util;
 
-import java.io.StringReader;
-
-import javax.xml.transform.stream.StreamSource;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.params.HttpClientParams;
+import cn.bc.web.ws.converter.WSConverter;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 import org.springframework.ws.transport.WebServiceMessageSender;
-import org.springframework.ws.transport.http.CommonsHttpMessageSender;
 import org.springframework.xml.transform.StringResult;
 
-import cn.bc.web.ws.converter.WSConverter;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
 
 /**
  * @author dragon
- * 
  */
 public class WSUtils {
 	public static <T> T sendAndReceive(String soapUrl, String soapAction,
-			String msgTpl, Object[] msgArgs, WSConverter<T> converter) {
+	                                   String msgTpl, Object[] msgArgs, WSConverter<T> converter) {
 		if (msgArgs != null && msgArgs.length > 0)
 			msgTpl = java.text.MessageFormat.format(msgTpl, msgArgs);
 		StreamSource source = new StreamSource(new StringReader(msgTpl));
@@ -36,7 +30,7 @@ public class WSUtils {
 	}
 
 	public static String sendAndReceive(String soapUrl, String soapAction,
-			String msgTpl, Object[] msgArgs) {
+	                                    String msgTpl, Object[] msgArgs) {
 		if (msgArgs != null && msgArgs.length > 0)
 			msgTpl = java.text.MessageFormat.format(msgTpl, msgArgs);
 		StreamSource source = new StreamSource(new StringReader(msgTpl));
@@ -51,18 +45,14 @@ public class WSUtils {
 	protected static WebServiceTemplate buildWebServiceTemplate() {
 		WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
 
-		HttpClientParams params = new HttpClientParams();
-		// 超时控制:milliseconds
-		params.setSoTimeout(30000);
+//		HttpClient httpClient = new DefaultHttpClient();
+//		// 超时控制:milliseconds
+//		httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 30000);//连接时间
+//		httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 30000);//数据传输时间
 
-		HttpClient httpClient = new HttpClient();
-		httpClient.setParams(params);
-
-		WebServiceMessageSender messageSender = new CommonsHttpMessageSender(
-				httpClient);
-
+		// TODO
+		WebServiceMessageSender messageSender = null;//new CommonsHttpMessageSender(null);
 		webServiceTemplate.setMessageSender(messageSender);
-
 		return webServiceTemplate;
 	}
 
