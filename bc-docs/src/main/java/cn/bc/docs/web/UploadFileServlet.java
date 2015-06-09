e00ab966fe0bd540706ec9b44c566a36c43cdcb3
dragon <rongjihuang@gmail.com>
2015-06-09 16:20:14 +0800

    升级到commons-lang3的编译问题修正

--- bc-docs/src/main/java/cn/bc/docs/web/UploadFileServlet.java
+++ bc-docs/src/main/java/cn/bc/docs/web/UploadFileServlet.java
@@ -14,7 +14,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
