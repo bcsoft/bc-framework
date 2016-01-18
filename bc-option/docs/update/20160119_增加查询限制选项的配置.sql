181c309f962bf9c945fb424ffbc86bb1c8b424ba
892106902@qq.com <892106902@qq.com>
2016-01-18 18:30:59 +0800

    20160119_增加查询限制选项的配置

---
+++ bc-option/docs/update/20160119_增加查询限制选项的配置.sql
@@ -0,0 +1,11 @@
+-- 清空数据
+delete from bc_option_item where pid = (select id from bc_option_group where key_ = 'search.limit');
+delete from bc_option_group where key_ = 'fuzzy.max.results';
+
+-- 单据样式 select * from bc_option_item where pid = (select id from bc_option_group where key_ = 'financial.receipt');
+insert into bc_option_group(id, key_, value_, order_, icon)
+	values (nextval('CORE_SEQUENCE'), 'search.limit', '查询限制', '5222', NULL);
+insert into bc_option_item(id, pid, status_, key_, value_, order_)
+	values (nextval('CORE_SEQUENCE'), (select id from bc_option_group where key_ = 'search.limit')
+	, 0, 'fuzzy.max.results', '25', '5223');
+
