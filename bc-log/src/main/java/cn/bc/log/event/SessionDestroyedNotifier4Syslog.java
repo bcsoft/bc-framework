da9d877e981b5e994427a1b72b01639f83db80b6
dragon <rongjihuang@gmail.com>
2016-07-21 09:38:37 +0800

    修正重启服务时 session 超时对应的空指针异常

--- bc-log/src/main/java/cn/bc/log/event/SessionDestroyedNotifier4Syslog.java
+++ bc-log/src/main/java/cn/bc/log/event/SessionDestroyedNotifier4Syslog.java
@@ -1,31 +1,27 @@
 /**
- * 
+ *
  */
 package cn.bc.log.event;
-
-import java.util.Calendar;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 
 import cn.bc.identity.event.LoginEvent;
 import cn.bc.identity.web.SystemContext;
 import cn.bc.log.domain.Syslog;
 import cn.bc.log.service.SyslogService;
 import cn.bc.web.event.SessionDestroyedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+
+import java.util.Calendar;
 
 /**
  * Session销毁的事件监听处理：记录超时日志
- * 
+ *
  * @author dragon
- * 
  */
-public class SessionDestroyedNotifier4Syslog implements
-		ApplicationListener<SessionDestroyedEvent> {
-	private static Log logger = LogFactory
-			.getLog(SessionDestroyedNotifier4Syslog.class);
+public class SessionDestroyedNotifier4Syslog implements ApplicationListener<SessionDestroyedEvent> {
+	private static Logger logger = LoggerFactory.getLogger(SessionDestroyedNotifier4Syslog.class);
 	private SyslogService syslogService;
 
 	@Autowired
@@ -36,12 +32,9 @@ public class SessionDestroyedNotifier4Syslog implements
 	public void onApplicationEvent(SessionDestroyedEvent event) {
 		SystemContext context = (SystemContext) event.getContext();
 		if (context != null) {// 表明之前用户已经处于登录状态
-			LoginEvent loginEvent = (LoginEvent) event.getSession()
-					.getAttribute("loginEvent");
+			LoginEvent loginEvent = (LoginEvent) event.getSession().getAttribute("loginEvent");
 			if (logger.isDebugEnabled())
-				logger.debug("session out with user="
-						+ context.getUserHistory().getName() + ",sid="
-						+ event.getSession().getId());
+				logger.debug("session out with user={}, sid=", context.getUserHistory().getName(), event.getSession().getId());
 			// 记录超时日志
 			Syslog log = new Syslog();
 			log.setType(Syslog.TYPE_LOGIN_TIMEOUT);
@@ -50,20 +43,21 @@ public class SessionDestroyedNotifier4Syslog implements
 			log.setSubject(context.getUserHistory().getName() + "登录超时");
 			log.setSid(event.getSid());
 
-			log.setServerIp(loginEvent.getServerIp());
-			log.setServerName(loginEvent.getServerName());
-			log.setServerInfo(loginEvent.getServerInfo());
+			if (loginEvent != null) {
+				log.setServerIp(loginEvent.getServerIp());
+				log.setServerName(loginEvent.getServerName());
+				log.setServerInfo(loginEvent.getServerInfo());
 
-			log.setClientIp(loginEvent.getClientIp());
-			log.setClientName(loginEvent.getClientName());
-			log.setClientInfo(loginEvent.getClientInfo());
-			log.setClientMac(loginEvent.getClientMac());
+				log.setClientIp(loginEvent.getClientIp());
+				log.setClientName(loginEvent.getClientName());
+				log.setClientInfo(loginEvent.getClientInfo());
+				log.setClientMac(loginEvent.getClientMac());
+			}
 
 			this.syslogService.save(log);
 		} else {
 			if (logger.isDebugEnabled())
-				logger.debug("session out without context. sid="
-						+ event.getSession().getId());
+				logger.debug("session out without context. sid={}", event.getSession().getId());
 		}
 	}
 }
