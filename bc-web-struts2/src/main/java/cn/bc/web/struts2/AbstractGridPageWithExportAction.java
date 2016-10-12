4a80035a462d16ee295a53d6adfbd472b6af7e9e
dragon <rongjihuang@gmail.com>
2016-10-12 15:43:14 +0800

    Polishing

--- bc-web-struts2/src/main/java/cn/bc/web/struts2/AbstractGridPageWithExportAction.java
+++ bc-web-struts2/src/main/java/cn/bc/web/struts2/AbstractGridPageWithExportAction.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package cn.bc.web.struts2;
 
 import cn.bc.Context;
@@ -9,11 +6,11 @@ import cn.bc.core.util.DateUtils;
 import cn.bc.web.struts2.event.ExportViewDataEvent;
 import cn.bc.web.ui.html.grid.*;
 import cn.bc.web.util.WebUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.ServletActionContext;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.util.StringUtils;
@@ -22,18 +19,16 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 带导出功能的视图的抽象Action
  *
  * @author dragon
  */
-public abstract class AbstractGridPageWithExportAction<T extends Object>
-		extends AbstractGridPageAction<T> implements
-		ApplicationEventPublisherAware {
+public abstract class AbstractGridPageWithExportAction<T extends Object> extends AbstractGridPageAction<T> implements ApplicationEventPublisherAware {
 	private static final long serialVersionUID = 1L;
-	private final static Log logger = LogFactory
-			.getLog("cn.bc.web.struts2.AbstractGridPageWithExportAction");
+	private final static Logger logger = LoggerFactory.getLogger("cn.bc.web.struts2.AbstractGridPageWithExportAction");
 	private ApplicationEventPublisher eventPublisher;
 
 	// 导出视图数据需要的变量
@@ -50,8 +45,7 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	public String exportFormat;
 	public String[] field;
 
-	public void setApplicationEventPublisher(
-			ApplicationEventPublisher applicationEventPublisher) {
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
 		this.eventPublisher = applicationEventPublisher;
 	}
 
@@ -59,17 +53,13 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	public String export() throws Exception {
 		Date startTime = new Date();
 		// 确定下载文件的名称(解决跨浏览器中文文件名乱码问题)
-		if (this.fileName == null || this.fileName.length() == 0)
-			this.fileName = this.getDefaultExportFileName();
+		if (this.fileName == null || this.fileName.length() == 0) this.fileName = this.getDefaultExportFileName();
+
 		String title = this.fileName;
-		this.fileName = WebUtils.encodeFileName(
-				ServletActionContext.getRequest(), this.fileName);
+		this.fileName = WebUtils.encodeFileName(ServletActionContext.getRequest(), this.fileName);
 
 		// 确定下载文件处理方法
-		// contentDisposition = "inline;filename=\"" +fileName
-		// +".xls\"";//在页面中打开
-		this.contentDisposition = "attachment;filename=\"" + fileName
-				+ ".xls\"";// 以附件方式下载
+		this.contentDisposition = "attachment;filename=\"" + fileName + ".xls\"";// 以附件方式下载
 
 		if (logger.isDebugEnabled()) {
 			logger.debug("exportKeys=" + exportKeys);
@@ -80,30 +70,25 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 		}
 
 		// TODO 根据导出条件构建
-		if (this.getPage() != null) {// 分页的处理
+		if (this.getPage() != null) {   // 分页的处理
 			this.setPage(this.findPage());
 			this.es = this.getPage().getData();
-		} else {// 非分页的处理
+		} else {                        // 非分页的处理
 			this.es = this.findList();
 		}
 
-		// 导出数据到Excel
-		if (logger.isDebugEnabled())
-			logger.debug("1:" + DateUtils.getWasteTime(startTime));
-		GridExporter exporter = buileGridExporter(title,
-				getText("label.idLabel"));
+		// 导出数据到 Excel
+		if (logger.isDebugEnabled()) logger.debug("1:" + DateUtils.getWasteTime(startTime));
+		GridExporter exporter = buileGridExporter(title, getText("label.idLabel"));
 		ByteArrayOutputStream out = new ByteArrayOutputStream(this.bufferSize);
-		if (logger.isDebugEnabled())
-			logger.debug("2:" + DateUtils.getWasteTime(startTime));
+		if (logger.isDebugEnabled()) logger.debug("2:" + DateUtils.getWasteTime(startTime));
 		exporter.exportTo(out);
-		if (logger.isDebugEnabled())
-			logger.debug("3:" + DateUtils.getWasteTime(startTime));
+		if (logger.isDebugEnabled()) logger.debug("3:" + DateUtils.getWasteTime(startTime));
 		byte[] data = out.toByteArray();
 		this.inputStream = new ByteArrayInputStream(data);
 
 		// 发布用户导出数据事件
-		ExportViewDataEvent event = new ExportViewDataEvent(this,
-				getModuleType(), "0", title, "xls", data);
+		ExportViewDataEvent event = new ExportViewDataEvent(this, getModuleType(), "0", title, "xls", data);
 		this.eventPublisher.publishEvent(event);
 
 		return "export";
@@ -119,8 +104,6 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 
 	/**
 	 * 获取模块的标识字符串，通常为Domain的类名
-	 *
-	 * @return
 	 */
 	protected String getModuleType() {
 		return StringUtils.capitalize(this.getFormActionName());
@@ -131,15 +114,13 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	 *
 	 * @param title   标题
 	 * @param idLabel id列的标题名称
-	 * @return
 	 */
 	protected GridExporter buileGridExporter(String title, String idLabel) {
 		GridExporter exporter = new GridExporter();
-		exporter.setColumns(this.getExportColumns()).setTitle(title)
-				.setData(this.es).setIdLabel(idLabel);
+		exporter.setColumns(this.getExportColumns()).setTitle(title).setData(this.es).setIdLabel(idLabel);
 
 		// 扩展额外的导出数据
-		Map<String, Object> map = new HashMap<String, Object>();
+		Map<String, Object> map = new HashMap<>();
 		this.extendGridExportData(map);
 		exporter.addExtra(map);
 
@@ -155,8 +136,7 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	protected void extendGridExportData(Map<String, Object> extras) {
 		// 默认使用视图的扩展参数
 		JSONObject json = getGridExtrasData();
-		if (json == null || json.length() == 0)
-			return;
+		if (json == null || json.length() == 0) return;
 		Iterator keys = json.keys();
 		String key;
 		try {
@@ -175,18 +155,11 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 
 	/**
 	 * 获取需要导出到Excel的列配置，默认使用同视图的配置一样
-	 *
-	 * @return
 	 */
 	protected List<Column> getExportColumns() {
-		List<Column> columns = this.getGridColumns();
-		List<Column> eportColumns = new ArrayList<Column>();
-		for (Column column : columns) {
-			if (this.isExportColumn(column.getId())
-					|| column instanceof IdColumn)
-				eportColumns.add(column);
-		}
-		return eportColumns;
+		return this.getGridColumns().stream()
+				.filter(column -> this.isExportColumn(column.getId()) || column instanceof IdColumn)
+				.collect(Collectors.toList());
 	}
 
 	/**
@@ -202,17 +175,13 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	 * </ul>
 	 *
 	 * @param key 列的标识
-	 * @return
 	 */
 	protected boolean isExportColumn(String key) {
-		return this.exportKeys == null || this.exportKeys.length() == 0
-				|| this.exportKeys.indexOf(key) != -1;
+		return this.exportKeys == null || this.exportKeys.length() == 0 || this.exportKeys.contains(key);
 	}
 
 	/**
 	 * 获取默认的导出文件名
-	 *
-	 * @return
 	 */
 	protected String getDefaultExportFileName() {
 		return getText("export.default.fileName") + getText(StringUtils.uncapitalize(getModuleType()));// 默认的文件名
@@ -221,8 +190,7 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 	@Override
 	protected void extendGridFooterButton(GridFooter gridFooter) {
 		// 导出按钮
-		if (this.canExport())
-			gridFooter.addButton(getDefaultExportButton());
+		if (this.canExport()) gridFooter.addButton(getDefaultExportButton());
 
 		// 导入按钮
 		gridFooter.addButton(this.getGridFooterImportButton());
@@ -233,25 +201,19 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 
 	/**
 	 * 判断用户能否导出数据：非移动设备默认可以导出，对移动设备只有岗位"在移动设备中下载数据"内的人可以导出
-	 *
-	 * @return
 	 */
 	protected boolean canExport() {
 		Context c = this.getContext();
-		if (new Boolean(true).equals(c.getAttr("mobile"))) {// 移动设备
+		if (Boolean.TRUE.equals(c.getAttr("mobile"))) {// 移动设备
 			@SuppressWarnings("unchecked")
-			List<String> groups = (List<String>) c.getAttr("groups");
-			if (groups == null)
-				return false;
-			return groups.contains("DownloadFromMobile");
+			List<String> groups = c.getAttr("groups");
+			return groups != null && groups.contains("DownloadFromMobile");
 		}
 		return true;
 	}
 
 	/**
 	 * 默认的导出按钮
-	 *
-	 * @return
 	 */
 	protected FooterButton getDefaultExportButton() {
 		return GridFooter.getDefaultExportButton(getText("label.export"));
@@ -259,8 +221,6 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 
 	/**
 	 * 获取导入按钮的配置，默认没有导入按钮
-	 *
-	 * @return
 	 */
 	protected FooterButton getGridFooterImportButton() {
 		return null;
@@ -268,8 +228,6 @@ public abstract class AbstractGridPageWithExportAction<T extends Object>
 
 	/**
 	 * 获取导入按钮的配置，默认没有导入按钮
-	 *
-	 * @return
 	 */
 	protected FooterButton getDefaultGridFooterImportButton() {
 		return GridFooter.getDefaultImportButton(getText("label.import"));
