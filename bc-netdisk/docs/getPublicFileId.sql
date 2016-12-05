51a760286f5c4308bb0779359bd1f8a555166830
dragon <rongjihuang@gmail.com>
2016-12-05 16:43:41 +0800

    修正网络硬盘打开错误

--- bc-netdisk/docs/getPublicFileId.sql
+++ bc-netdisk/docs/getPublicFileId.sql
@@ -1,12 +1,15 @@
 drop function getPublicFileId();
 create or replace function getPublicFileId()
-  returns character varying as
+  returns text as
 $BODY$
 declare
 	--定义变量
-	fileId varchar(4000);
+	fileId text;
 begin
-	with recursive n as(select * from bc_netdisk_file where folder_type = 1 union select f.* from bc_netdisk_file f,n where f.pid=n.id)
+	with recursive n as (
+		select * from bc_netdisk_file where folder_type = 1
+		union select f.* from bc_netdisk_file f, n where f.pid=n.id
+	)
 	select string_agg(id||'',',') into fileId from n;
 
 	return fileId;
