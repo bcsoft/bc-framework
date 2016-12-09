0257930253fdd8c6f6912a92c2e669a6fc8a663f
dragon <rongjihuang@gmail.com>
2016-12-09 17:23:47 +0800

    AdvanceCondition 增加 toCondition 和 toConditions 工具方法，方便新的高级搜索条件的生成

--- bc-core/src/main/java/cn/bc/core/query/condition/AdvanceCondition.java
+++ bc-core/src/main/java/cn/bc/core/query/condition/AdvanceCondition.java
@@ -1,9 +1,16 @@
-/**
- *
- */
 package cn.bc.core.query.condition;
 
+import cn.bc.core.exception.CoreException;
 import cn.bc.core.query.QueryOperator;
+import cn.bc.core.query.condition.impl.SimpleAdvanceCondition;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 高级条件配置
@@ -27,4 +34,31 @@ public interface AdvanceCondition {
 	 * @return 参数值
 	 */
 	Object getValue();
+
+	/**
+	 * 根据配置初始化一个条件实例
+	 *
+	 * @param one 单个条件的配置，数据结构为 [id, value[, type, operator]]
+	 * @return 条件实例
+	 */
+	static AdvanceCondition toCondition(JsonArray one) {
+		return new SimpleAdvanceCondition(one);
+	}
+
+	/**
+	 * 根据配置初始化条件集
+	 *
+	 * @param multi 多个条件配置的 json 数组字符串，数据结构为 [[id, value[, type, operator]], ...]
+	 * @return 条件集
+	 */
+	static List<AdvanceCondition> toConditions(String multi) {
+		List<AdvanceCondition> all = new ArrayList<>();
+		if (multi != null) try {
+			Json.createReader(new StringReader(URLDecoder.decode(multi, "UTF-8"))).readArray()
+					.forEach(item -> all.add(toCondition((JsonArray) item)));
+		} catch (UnsupportedEncodingException e) {
+			throw new CoreException(e.getMessage(), e);
+		}
+		return all;
+	}
 }
