ee686fe479781f90f9c1cf9607b784da76bef9a5
BallsperCat <13430280541@163.com>
2017-12-19 15:36:55 +0800

    添加更新系统发件邮箱配置 SQL 脚本

---
+++ bc-message/docs/update/20171219_更新发件邮箱配置.sql
@@ -0,0 +1,6 @@
+﻿update bc_option_item set value_ = '13318786102@139.com'
+  where pid = (select id from bc_option_group where key_ = 'bc.mailSender') and key_ = 'username';
+update bc_option_item set value_ = 'gf81800088'
+  where pid = (select id from bc_option_group where key_ = 'bc.mailSender') and key_ = 'password';
+update bc_option_item set value_ = 'smtp.139.com'
+  where pid = (select id from bc_option_group where key_ = 'bc.mailSender') and key_ = 'host';
