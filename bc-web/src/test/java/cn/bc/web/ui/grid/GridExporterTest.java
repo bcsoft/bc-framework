f23949c74259c09dde2b8c01ba3d97e37c4faa26
RJ <rongjihuang@gmail.com>
2021-12-20 14:40:10 +0800

    bc-web 视图导出 Excel 改用 jxls2
    1. 添加了适应 jxls2 的 export.xlsx 通用导出 Excel 模板。
    2. 需要同时添加 poi-ooxml-schemas 依赖，否则导出 Excel 会出现如下错误：
    Caused by: java.lang.ClassNotFoundException: org.apache.xmlbeans.XmlObject

--- bc-web/src/test/java/cn/bc/web/ui/grid/GridExporterTest.java
+++ bc-web/src/test/java/cn/bc/web/ui/grid/GridExporterTest.java
@@ -1,11 +1,14 @@
 package cn.bc.web.ui.grid;
 
 import cn.bc.core.util.DateUtils;
-import net.sf.jxls.transformer.XLSTransformer;
+import org.junit.Assert;
 import org.junit.Test;
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.*;
 
@@ -17,16 +20,20 @@ public class GridExporterTest {
    */
   @Test
   public void test01() throws Exception {
-    Map<String, Object> map = buildData();
-    XLSTransformer transformer = new XLSTransformer();
     // 使用默认的模板
-    InputStream is = new ClassPathResource("cn/bc/web/template/export.xls")
+    InputStream is = new ClassPathResource("cn/bc/web/template/export.xlsx")
       .getInputStream();
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(4096);
-    transformer.transformXLS(is, map).write(outputStream);
-    is.close();
-    outputStream.flush();
-    outputStream.close();
+
+    Map<String, Object> map = buildData();
+    File fileOut = new File("target/export.xlsx");
+    if (fileOut.exists()) fileOut.delete();
+    JxlsHelper.getInstance().processTemplate(
+      is,
+      new FileOutputStream(fileOut),
+      new Context(map)
+    );
+    Assert.assertTrue(fileOut.exists());
+    Assert.assertTrue(fileOut.length() > 0);
   }
 
   private Map<String, Object> buildData() {
